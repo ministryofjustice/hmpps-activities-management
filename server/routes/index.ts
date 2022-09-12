41fa6db00f6bc020fe954926580392d260112e79
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2022-09-12 12:30:23 +0100

    SAA-15: Disable lint checl for unused variable for services - will be used shortly

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import type { Services } from '../services'
 import homeRoutes from './home'
 
+// Services wil be passed for most routes - to come
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(service: Services): Router {
   const router = Router({ mergeParams: true })
   router.use(homeRoutes())
