0b50aa889fcd44c62282a4fdc6dd586943268448
Stephen McVeigh <smcveigh941@gmail.com>
2022-10-05 11:10:34 +0100

    SAA-25: Fix auto-selected location on change location page (#18)

--- integration_tests/pages/changeLocationPage.ts
+++ integration_tests/pages/changeLocationPage.ts
@@ -5,12 +5,22 @@ export default class ChangeLocationPage extends Page {
     super('change-location-page')
   }
 
-  caseLoadOptions = (): Cypress.Chainable<string[]> =>
+  locationOptions = (): Cypress.Chainable<string[]> =>
     cy
-      .get('#changeCaseloadSelect')
+      .get('#changeLocationSelect')
       .find('option')
       .then($el => {
         return Cypress.$.makeArray($el).map(el => el.innerText)
+      })
+
+  selectedLocation = (): Cypress.Chainable<string> =>
+    cy
+      .get('#changeLocationSelect')
+      .find('option:selected')
+      .then($el => {
+        return Cypress.$.makeArray($el)
+          .map(el => el.innerText)
+          .pop()
       })
 
   submit = (): PageElement => cy.get('#btnSubmit')
