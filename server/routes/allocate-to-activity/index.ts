68b6486995a3c867f152dee2ceaf8981efb2d849
Mark Ford <mark.ford@digital.justice.gov.uk>
2022-12-08 11:52:50 +0000

    SAA-212 Identify candidates
    tweak

--- server/routes/allocate-to-activity/index.ts
+++ server/routes/allocate-to-activity/index.ts
@@ -3,7 +3,7 @@ import asyncMiddleware from '../../middleware/asyncMiddleware'
 import CategoriesRoutes from './handlers/categories'
 import { Services } from '../../services'
 import ActivitiesRoutes from './handlers/activities'
-// import SchedulesRoutes from './handlers/schedules'
+import SchedulesRoutes from './handlers/schedules'
 import candidatesRoutes from './candidates'
 
 export default function Index(services: Services): Router {
@@ -13,11 +13,11 @@ export default function Index(services: Services): Router {
 
   const categoriesHandler = new CategoriesRoutes(services.activitiesService, services.capacitiesService)
   const activitiesHandler = new ActivitiesRoutes(services.activitiesService, services.capacitiesService)
-  // const schedulesHandler = new SchedulesRoutes(services.activitiesService, services.capacitiesService)
+  const schedulesHandler = new SchedulesRoutes(services.activitiesService, services.capacitiesService)
 
   get('/categories', categoriesHandler.GET)
   get('/categories/:categoryId/activities', activitiesHandler.GET)
-  // get('/activities/:activityId/schedules', schedulesHandler.GET)
+  get('/activities/:activityId/schedules', schedulesHandler.GET)
 
   router.use('/:scheduleId/candidates', candidatesRoutes(services))
   return router
