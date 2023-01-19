f363e560ec60bfff46d9d95eeb7d7931cbd40b6e
Stephen Mc Veigh <smcveigh941@gmail.com>
2023-01-19 17:32:00 +0000

    SAA-312: Print layout spike

--- assets/js/govukFrontendInit.js
+++ assets/js/govukFrontendInit.js
@@ -2,11 +2,16 @@ window.GOVUKFrontend.initAll()
 window.MOJFrontend.initAll()
 
 // Initiate the back links
-$('[class$=js-backlink]').on('click', e => {
+$('[class*=js-backlink]').on('click', e => {
   e.preventDefault()
   if ($('ul.govuk-error-summary__list').length > 0) {
     window.history.go(-2)
   } else {
     window.history.go(-1)
   }
+})
+
+$('[class*=js-print]').on('click', e => {
+  e.preventDefault()
+  window.print()
 })
