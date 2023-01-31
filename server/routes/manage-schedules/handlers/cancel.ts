361d87e7b65bf8e7d711617b7fc7256a006577be
gerry.allan <gerry.allan@digital.justice.gov.uk>
2023-01-31 10:02:04 +0000

    SAA-284: New view activity page

--- server/routes/manage-schedules/handlers/cancel.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-
-export default class CancelRoutes {
-  GET = async (req: Request, res: Response): Promise<void> => {
-    res.redirect('activities')
-  }
-}
