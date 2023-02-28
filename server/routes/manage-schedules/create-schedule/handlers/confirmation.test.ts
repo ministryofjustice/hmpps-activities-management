95dda801eea98a52a4dbca2ae69c1af63701b283
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-02-28 16:49:16 +0000

    SAA-364: Fixed confirmation page activity name

--- server/routes/manage-schedules/create-schedule/handlers/confirmation.test.ts
+++ server/routes/manage-schedules/create-schedule/handlers/confirmation.test.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from 'express'
+import { when } from 'jest-when'
+import atLeast from '../../../../../jest.setup'
+import activity from '../../../../middleware/fixtures/activity_1.json'
 import ConfirmationRoutes from './confirmation'
+import ActivitiesService from '../../../../services/activitiesService'
+
+jest.mock('../../../../services/activitiesService')
+
+const activitiesService = new ActivitiesService(null, null, null) as jest.Mocked<ActivitiesService>
 
 describe('Route Handlers - Create an activity schedule - Confirmation', () => {
-  const handler = new ConfirmationRoutes()
+  const handler = new ConfirmationRoutes(activitiesService)
   let req: Request
   let res: Response
 
@@ -37,6 +45,8 @@ describe('Route Handlers - Create an activity schedule - Confirmation', () => {
 
   describe('GET', () => {
     it('should render the expected page', async () => {
+      when(activitiesService.getActivity).calledWith(atLeast(1)).mockResolvedValueOnce(activity)
+
       await handler.GET(req, res)
 
       expect(res.render).toHaveBeenCalledWith('pages/manage-schedules/create-schedule/confirmation', {
--- server/routes/manage-schedules/create-schedule/handlers/confirmation.ts
+++ server/routes/manage-schedules/create-schedule/handlers/confirmation.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express'
+import ActivitiesService from '../../../../services/activitiesService'
 
 export default class ConfirmationRoutes {
+  constructor(private readonly activitiesService: ActivitiesService) {}
+
   GET = async (req: Request, res: Response): Promise<void> => {
+    const { user } = res.locals
     const scheduleId = +req.params.scheduleId
 
     res.render('pages/manage-schedules/create-schedule/confirmation', { scheduleId, activity: res.locals.activity })
