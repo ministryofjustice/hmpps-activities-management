b61ba5feb585242db0544a814e9dca8946c2fa8e
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-03-01 15:26:38 +0000

    SAA-377 introduction of category filter on the activites dasboard to pin down to specfic activity types.

--- server/routes/manage-schedules/handlers/activities.test.ts
+++ server/routes/manage-schedules/handlers/activities.test.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express'
 
+import { when } from 'jest-when'
 import ActivitiesService from '../../../services/activitiesService'
 import ActivitiesRoutes from './activities'
-import { ActivityLite } from '../../../@types/activitiesAPI/types'
+import { ActivityCategory, ActivityLite } from '../../../@types/activitiesAPI/types'
+import atLeast from '../../../../jest.setup'
 
 jest.mock('../../../services/activitiesService')
 
@@ -14,16 +16,36 @@ describe('Route Handlers - Activities dashboard', () => {
   let res: Response
 
   const mockActivitiesData = () => {
+    const educationCategory = {
+      id: 1,
+      name: 'Education',
+    } as ActivityCategory
+
+    const gymCategory = {
+      id: 2,
+      name: 'Gym, sport, fitness',
+    } as ActivityCategory
+
     const maths = {
       id: 1,
       summary: 'Maths level 1',
+      category: educationCategory,
     } as ActivityLite
+
     const english = {
       id: 2,
       summary: 'English level 1',
+      category: educationCategory,
     } as ActivityLite
 
-    activitiesService.getActivities.mockResolvedValue([maths, english])
+    const gym = {
+      id: 3,
+      summary: 'Gym',
+      category: gymCategory,
+    } as ActivityLite
+
+    activitiesService.getActivities.mockResolvedValue([maths, english, gym])
+    activitiesService.getActivityCategories.mockResolvedValue([educationCategory, gymCategory])
   }
 
   beforeEach(() => {
@@ -34,7 +56,9 @@ describe('Route Handlers - Activities dashboard', () => {
       render: jest.fn(),
     } as unknown as Response
 
-    req = {} as unknown as Request
+    req = {
+      query: {},
+    } as unknown as Request
 
     mockActivitiesData()
   })
@@ -44,21 +68,153 @@ describe('Route Handlers - Activities dashboard', () => {
   })
 
   describe('GET', () => {
-    it('should render activities', async () => {
-      req.params = { categoryId: '1' }
+    it('should render all activities', async () => {
+      req.query = { categoryFilter: 'all' }
 
       await handler.GET(req, res)
       expect(res.render).toHaveBeenCalledWith('pages/manage-schedules/activities-dashboard', {
-        activities: expect.arrayContaining([
+        activities: [
           {
+            category: {
+              id: 1,
+              name: 'Education',
+            },
+            summary: 'Maths level 1',
+            id: 1,
+          },
+          {
+            category: {
+              id: 1,
+              name: 'Education',
+            },
             summary: 'English level 1',
             id: 2,
           },
           {
+            category: {
+              id: 2,
+              name: 'Gym, sport, fitness',
+            },
+            summary: 'Gym',
+            id: 3,
+          },
+        ],
+        categories: [
+          {
+            id: 1,
+            name: 'Education',
+          },
+          {
+            id: 2,
+            name: 'Gym, sport, fitness',
+          },
+        ],
+        filters: { categoryFilter: 'all' },
+      })
+    })
+
+    it('should render education activities only', async () => {
+      const educationCategory = {
+        id: 1,
+        name: 'Education',
+      } as ActivityCategory
+
+      when(activitiesService.getActivitiesInCategory)
+        .calledWith(atLeast(1))
+        .mockResolvedValue([
+          {
+            id: 1,
+            summary: 'Maths level 1',
+            category: educationCategory,
+          } as ActivityLite,
+          {
+            id: 2,
+            summary: 'English level 1',
+            category: educationCategory,
+          } as ActivityLite,
+        ])
+
+      req.query = { categoryFilter: '1' }
+
+      await handler.GET(req, res)
+      expect(res.render).toHaveBeenCalledWith('pages/manage-schedules/activities-dashboard', {
+        activities: [
+          {
+            category: {
+              id: 1,
+              name: 'Education',
+            },
             summary: 'Maths level 1',
             id: 1,
           },
-        ]),
+          {
+            category: {
+              id: 1,
+              name: 'Education',
+            },
+            summary: 'English level 1',
+            id: 2,
+          },
+        ],
+        categories: [
+          {
+            id: 1,
+            name: 'Education',
+          },
+          {
+            id: 2,
+            name: 'Gym, sport, fitness',
+          },
+        ],
+        filters: {
+          categoryFilter: '1',
+        },
+      })
+    })
+
+    it('should render gym activities only', async () => {
+      const gymCategory = {
+        id: 2,
+        name: 'Gym, sport, fitness',
+      } as ActivityCategory
+
+      when(activitiesService.getActivitiesInCategory)
+        .calledWith(atLeast(2))
+        .mockResolvedValue([
+          {
+            id: 3,
+            summary: 'Gym',
+            category: gymCategory,
+          } as ActivityLite,
+        ])
+
+      req.query = { categoryFilter: '2' }
+
+      await handler.GET(req, res)
+      expect(res.render).toHaveBeenCalledWith('pages/manage-schedules/activities-dashboard', {
+        activities: [
+          {
+            category: {
+              id: 2,
+              name: 'Gym, sport, fitness',
+            },
+            summary: 'Gym',
+            id: 3,
+          },
+        ],
+        categories: [
+          {
+            id: 1,
+            name: 'Education',
+          },
+          {
+            id: 2,
+            name: 'Gym, sport, fitness',
+          },
+        ],
+        filters: {
+          categoryFilter: '2',
+        },
       })
     })
   })
--- server/routes/manage-schedules/handlers/activities.ts
+++ server/routes/manage-schedules/handlers/activities.ts
@@ -1,14 +1,29 @@
 import { Request, Response } from 'express'
 import ActivitiesService from '../../../services/activitiesService'
 
+type Filters = {
+  categoryFilter: string
+}
+
 export default class ActivitiesRoutes {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
   GET = async (req: Request, res: Response): Promise<void> => {
     const { user } = res.locals
-    const activities = await this.activitiesService.getActivities(user)
+    const filters = req.query as Filters
+
+    const [activities, categories] = await Promise.all([
+      !filters.categoryFilter || filters.categoryFilter === 'all'
+        ? this.activitiesService.getActivities(user)
+        : this.activitiesService.getActivitiesInCategory(+filters.categoryFilter, user),
+
+      this.activitiesService.getActivityCategories(user),
+    ])
+
     res.render('pages/manage-schedules/activities-dashboard', {
       activities,
+      categories,
+      filters,
     })
   }
 }
