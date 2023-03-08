35e9d49f5e38f402bc2a6776118465676b108cd4
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-03-08 18:22:36 +0000

    SAA-419: Additional unused files

--- server/routes/home/handlers/home.ts
+++ server/routes/home/handlers/home.ts
@@ -2,11 +2,6 @@ import { Request, Response } from 'express'
 
 export default class HomeRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
-    const { user } = res.locals
-    const viewContext = {
-      shouldShowAlphaPrisonActivityListAm: user.activeCaseLoad.isRolledOut,
-      shouldShowAlphaPrisonActivityListDps: !user.activeCaseLoad.isRolledOut,
-    }
-    res.render('pages/home/index', viewContext)
+    res.render('pages/home/index')
   }
 }
