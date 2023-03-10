ba1f77061fc08730688ed621cb6dc3eeec866797
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-03-10 17:17:20 +0000

    SAA-397: Cancel session confirmation

--- server/@types/recordAttendance.ts
+++
@@ -1,4 +0,0 @@
-export type SessionCancelationRequest = {
-  reason: string
-  comments?: string
-}
--- server/routes/record-attendance/handlers/cancelSession.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express'
-
-export default class CancelSessionRoutes {
-  GET = async (req: Request, res: Response) => res.render('pages/record-attendance/cancel-reason')
-}
