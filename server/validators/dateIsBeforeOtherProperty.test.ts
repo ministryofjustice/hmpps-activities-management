96185a371f8fb3724502efbda99484c556159ec1
gerry.allan <gerry.allan@digital.justice.gov.uk>
2023-03-13 09:07:14 +0000

    SAA-396: Add page to capture reasons for non-attendance

--- server/validators/dateIsBeforeOtherProperty.test.ts
+++ server/validators/dateIsBeforeOtherProperty.test.ts
@@ -30,19 +30,19 @@ describe('dateIsBeforeOtherProperty', () => {
   })
 
   it('should fail validation for a date equal to the other date', async () => {
-    // const body = {
-    //   date: plainToInstance(SimpleDate, {
-    //     day: 21,
-    //     month: 12,
-    //     year: 2022,
-    //   }),
-    //   otherDate: '2022-12-21',
-    // }
-    //
-    // const requestObject = plainToInstance(DummyForm, body)
-    // const errors = await validate(requestObject).then(errs => errs.flatMap(associateErrorsWithProperty))
-    //
-    // expect(errors).toEqual([{ property: 'date', error: 'Enter date before the other date' }])
+    const body = {
+      date: plainToInstance(SimpleDate, {
+        day: 21,
+        month: 12,
+        year: 2022,
+      }),
+      otherDate: '2022-12-21',
+    }
+
+    const requestObject = plainToInstance(DummyForm, body)
+    const errors = await validate(requestObject).then(errs => errs.flatMap(associateErrorsWithProperty))
+
+    expect(errors).toEqual([{ property: 'date', error: 'Enter date before the other date' }])
   })
 
   it('should pass validation for a date before to the other date', async () => {
