295919cc4d6ded5517c4e062270da4d084d3e848
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-03-14 14:00:42 +0000

    SAA-410: Validate max occurrences based on frequency

--- server/routes/appointments/create-single/handlers/repeatPeriodAndCount.ts
+++ server/routes/appointments/create-single/handlers/repeatPeriodAndCount.ts
@@ -3,6 +3,7 @@ import { Expose, Type } from 'class-transformer'
 import { IsEnum, Min } from 'class-validator'
 import { AppointmentRepeatPeriod } from '../../../../@types/activitiesAPI/types'
 import { YesNo } from '../../../../@types/activities'
+import MaxWhenDependentPropertyValueIs from '../../../../validators/maxWhenDependentPropertyValueIs'
 
 export class RepeatPeriodAndCount {
   @Expose()
@@ -12,6 +13,22 @@ export class RepeatPeriodAndCount {
   @Expose()
   @Type(() => Number)
   @Min(1, { message: 'Enter how many times the appointment will repeat up to a maximum of one year' })
+  @MaxWhenDependentPropertyValueIs(260, 'repeatPeriod', AppointmentRepeatPeriod.WEEKDAY, {
+    message: 'Number of appointments must be $constraint1 or fewer',
+  })
+  @MaxWhenDependentPropertyValueIs(365, 'repeatPeriod', AppointmentRepeatPeriod.DAILY, {
+    message: 'Number of appointments must be $constraint1 or fewer',
+  })
+  @MaxWhenDependentPropertyValueIs(52, 'repeatPeriod', AppointmentRepeatPeriod.WEEKLY, {
+    message: 'Number of appointments must be $constraint1 or fewer',
+  })
+  @MaxWhenDependentPropertyValueIs(26, 'repeatPeriod', AppointmentRepeatPeriod.FORTNIGHTLY, {
+    message: 'Number of appointments must be $constraint1 or fewer',
+  })
+  @Min(1, { message: 'Enter how many times the appointment will repeat up to a maximum of one year' })
+  @MaxWhenDependentPropertyValueIs(12, 'repeatPeriod', AppointmentRepeatPeriod.MONTHLY, {
+    message: 'Number of appointments must be $constraint1 or fewer',
+  })
   repeatCount: number
 }
 
---
+++ server/validators/maxWhenDependentPropertyValueIs.ts
@@ -0,0 +1,24 @@
+import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator'
+
+export default function MaxWhenDependentPropertyValueIs(
+  maxValue: number,
+  dependentPropertyName: string,
+  validDependentPropertyValue: unknown,
+  validationOptions?: ValidationOptions,
+) {
+  return (object: unknown, propertyName: string) => {
+    registerDecorator({
+      name: 'maxWhenOtherPropertyValueIs',
+      target: object.constructor,
+      propertyName,
+      constraints: [maxValue, dependentPropertyName, validDependentPropertyValue],
+      options: validationOptions,
+      validator: {
+        validate(value: number, args: ValidationArguments) {
+          const dependentPropertyValue = args.object[dependentPropertyName]
+          return dependentPropertyValue === validDependentPropertyValue ? value <= maxValue : true
+        },
+      },
+    })
+  }
+}
