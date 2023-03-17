1523e069f68fbf816338bdbf8d89a62b01f05a00
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-03-17 12:48:07 +0000

    SAA-442: Appointment occurrence movement slip (#181)
    * SAA-442: Working implementation - needs tests
    
    * SAA-442: Movement slip e2e test
    
    * SAA-442: Router tests and comment on movement slip
    
    * SAA-442: View tests

--- server/routes/appointments/occurrence-details/index.ts
+++ server/routes/appointments/occurrence-details/index.ts
@@ -1,6 +1,7 @@
 import { RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../../middleware/asyncMiddleware'
 import OccurrenceDetailsRoutes from './handlers/occurrenceDetails'
+import OccurrenceMovementSlipRoutes from './handlers/occurrenceMovementSlip'
 import { Services } from '../../../services'
 
 export default function Index({ activitiesService }: Services): Router {
@@ -9,8 +10,10 @@ export default function Index({ activitiesService }: Services): Router {
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   const occurrenceDetailsHandler = new OccurrenceDetailsRoutes(activitiesService)
+  const occurrenceMovementSlipHandler = new OccurrenceMovementSlipRoutes(activitiesService)
 
   get('/:id', occurrenceDetailsHandler.GET)
+  get('/movement-slip/:id', occurrenceMovementSlipHandler.GET)
 
   return router
 }
