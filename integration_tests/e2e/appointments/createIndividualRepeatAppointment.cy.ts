d20584cf509bac8633b68bd95956ae4e3d041693
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-04-17 16:57:21 +0100

    SAA-468: Edit occurrence location

--- integration_tests/e2e/appointments/createIndividualRepeatAppointment.cy.ts
+++ integration_tests/e2e/appointments/createIndividualRepeatAppointment.cy.ts
@@ -111,8 +111,8 @@ context('Create individual repeat appointment', () => {
     appointmentDetailsPage.assertRepeatCount('2')
     appointmentDetailsPage.assertOccurrences(
       new Map([
-        [1, formatDate(tomorrow, 'd MMM yyyy')],
-        [2, formatDate(weekTomorrow, 'd MMM yyyy')],
+        [1, { date: formatDate(tomorrow, 'd MMM yyyy'), edited: false }],
+        [2, { date: formatDate(weekTomorrow, 'd MMM yyyy'), edited: true }],
       ]),
     )
     appointmentDetailsPage.assertCreatedBy('J. Smith')
