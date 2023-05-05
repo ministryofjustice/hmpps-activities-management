d3a68c755841388b6b4670fa6455cad373c4b0d0
Lee Jacobson <125488090+leejacobson-moj@users.noreply.github.com>
2023-05-05 12:22:39 +0100

    SAA-637: Added "in cell" location radio (#249)

--- server/routes/create-an-activity/handlers/location.ts
+++ server/routes/create-an-activity/handlers/location.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express'
 import _ from 'lodash'
-import { Expose, Type } from 'class-transformer'
-import { IsNotEmpty, IsNumber } from 'class-validator'
+import { Expose, Transform, Type } from 'class-transformer'
+import { IsEnum, IsNotEmpty, IsNumber, ValidateIf } from 'class-validator'
 import PrisonService from '../../../services/prisonService'
+
+export enum InCell {
+  IN_CELL = 'IN_CELL',
+  OUT_OF_CELL = 'OUT_OF_CELL',
+}
 
 export class Location {
   @Expose()
+  @IsEnum(InCell, { message: 'Select whether location is in-cell or out of cell' })
+  @Transform(({ value }) => InCell[value])
+  inCell: InCell
+
+  @Expose()
+  @ValidateIf(l => l.inCell === InCell.OUT_OF_CELL)
   @Type(() => Number)
   @IsNotEmpty({ message: 'Select a location' })
   @IsNumber({ allowNaN: false }, { message: 'Select a location' })
@@ -27,14 +38,21 @@ export default class LocationRoutes {
 
   POST = async (req: Request, res: Response): Promise<void> => {
     const { user } = res.locals
-    const location = await this.prisonService
-      .getEventLocations(user.activeCaseLoad.caseLoadId, user)
-      .then(locations => locations.find(l => l.locationId === req.body.location))
+    const { location, inCell } = req.body
 
-    req.session.createJourney.location = {
-      id: location.locationId,
-      name: location.userDescription,
+    if (inCell === InCell.OUT_OF_CELL) {
+      const locationResult = await this.prisonService
+        .getEventLocations(user.activeCaseLoad.caseLoadId, user)
+        .then(locations => locations.find(l => l.locationId === location))
+
+      req.session.createJourney.location = {
+        id: locationResult.locationId,
+        name: locationResult.userDescription,
+      }
+    } else {
+      req.session.createJourney.location = null
     }
+    req.session.createJourney.inCell = inCell === InCell.IN_CELL
 
     res.redirectOrReturn(`capacity`)
   }
