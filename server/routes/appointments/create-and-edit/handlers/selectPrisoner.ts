5c880716611b09fbb07ae919ade278326eaf05da
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-05-18 18:45:40 +0100

    SAA-560: Use GET for search

--- server/routes/appointments/create-and-edit/handlers/selectPrisoner.ts
+++ server/routes/appointments/create-and-edit/handlers/selectPrisoner.ts
@@ -3,18 +3,10 @@ import { Expose, Transform } from 'class-transformer'
 import { IsNotEmpty } from 'class-validator'
 import PrisonService from '../../../../services/prisonService'
 import { AppointmentJourneyMode, AppointmentType } from '../appointmentJourney'
-import { Prisoner } from '../../../../@types/prisonerOffenderSearchImport/types'
 
 export class PrisonerSearch {
   @Expose()
-  @IsNotEmpty({ message: 'Enter a name or prisoner number to search by' })
-  query: string
-
-  @Expose()
-  @Transform(({ value }) => value === 'true')
-  isSelection: boolean
-
-  @Expose()
+  @IsNotEmpty({ message: 'You must select one option' })
   selectedPrisoner: string
 }
 
@@ -22,58 +14,59 @@ export default class SelectPrisonerRoutes {
   constructor(private readonly prisonService: PrisonService) {}
 
   GET = async (req: Request, res: Response): Promise<void> => {
-    res.render('pages/appointments/create-and-edit/select-prisoner')
+    const { query, search } = req.query
+
+    // The "search" variable is used for form validation because it's always set when the form submits
+    // "query" can be undefined either because it's an inital page load or because no query term was entered
+    if (!search || search !== 'true') return res.render('pages/appointments/create-and-edit/select-prisoner')
+
+    const prisoners = await this.getPrisoners(req, res)
+    if (!prisoners) return
+    return res.render('pages/appointments/create-and-edit/select-prisoner', { prisoners, query })
   }
 
   POST = async (req: Request, res: Response): Promise<void> => {
-    const { query, isSelection } = req.body
+    const result = await this.addSelectedPrisonerToSession(req, res)
 
-    const prisoners = await this.getPrisoners(req, res)
-
-    if (prisoners && isSelection) {
-      const result = await this.addSelectedPrisonerToSession(req, prisoners)
-
-      if (result) {
-        if (req.session.appointmentJourney.type === AppointmentType.GROUP) {
-          return res.redirect('review-prisoners')
-        }
-        return res.redirectOrReturn('category')
+    if (result) {
+      if (req.session.appointmentJourney.type === AppointmentType.GROUP) {
+        return res.redirect('review-prisoners')
       }
-      return res.validationFailed('selectedPrisoner', 'You must select one option')
+      return res.redirectOrReturn('category')
     }
-
-    return res.render('pages/appointments/create-and-edit/select-prisoner', { prisoners, query })
+    return res.validationFailed('selectedPrisoner', 'You must select one option')
   }
 
   EDIT = async (req: Request, res: Response): Promise<void> => {
-    const { isSelection, query } = req.body
-
-    const prisoners = await this.getPrisoners(req, res)
-
-    if (prisoners && isSelection) {
-      const result = await this.addSelectedPrisonerToSession(req, prisoners)
-      if (result) return res.redirect('review-prisoners')
-      return res.validationFailed('selectedPrisoner', 'You must select one option')
-    }
-
-    return res.render('pages/appointments/create-and-edit/select-prisoner', { prisoners, query })
+    const result = await this.addSelectedPrisonerToSession(req, res)
+    
+    if (result) return res.redirect('review-prisoners')
+    
+    res.validationFailed('selectedPrisoner', 'You must select one option')
   }
 
   private getPrisoners = async (req: Request, res: Response) => {
-    const { query } = req.body
+    const query = req.query.query
     const { user } = res.locals
+
+    if (typeof query !== 'string' || query === '') {
+      res.validationFailed('query', 'Enter a name or prisoner number to search by')
+      return false
+    }
 
     const results = await this.prisonService.searchPrisonInmates(query, user)
 
     return results.content
   }
 
-  private addSelectedPrisonerToSession = async (req: Request, prisoners: Prisoner[]) => {
+  private addSelectedPrisonerToSession = async (req: Request, res: Response) => {
     const { selectedPrisoner } = req.body
+    const { user } = res.locals
 
     if (!selectedPrisoner || selectedPrisoner === '') return false
 
-    const prisoner = prisoners.find(p => p.prisonerNumber === selectedPrisoner)
+    const prisoner = await this.prisonService.getInmateByPrisonerNumber(selectedPrisoner, user)
+
     if (!prisoner) return false
 
     const prisonerData = {
