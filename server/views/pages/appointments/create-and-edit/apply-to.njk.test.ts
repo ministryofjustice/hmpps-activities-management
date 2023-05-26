6033b3bac9f678570649ca3c0bbdb7ef7b4017c6
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-05-26 16:51:41 +0100

    SAA-714: Add remove prisoners automated tests (#297)
    * SAA-704: Initial blank view test files
    
    * SAA-714: Apply to and confirm edit view tests
    
    * SAA-714: Confirm edit view tests
    
    * SAA-714: Comment view tests
    
    * SAA-714: Final view tests

---
+++ server/views/pages/appointments/create-and-edit/apply-to.njk.test.ts
@@ -0,0 +1,172 @@
+import * as cheerio from 'cheerio'
+import nunjucks, { Template } from 'nunjucks'
+import fs from 'fs'
+import { addDays } from 'date-fns'
+import { registerNunjucks } from '../../../../nunjucks/nunjucksSetup'
+import {
+  AppointmentJourney,
+  AppointmentJourneyMode,
+  AppointmentType,
+} from '../../../../routes/appointments/create-and-edit/appointmentJourney'
+import { AppointmentApplyTo, AppointmentApplyToOption } from '../../../../@types/appointments'
+import { formatDate } from '../../../../utils/utils'
+import { EditAppointmentJourney } from '../../../../routes/appointments/create-and-edit/editAppointmentJourney'
+
+const view = fs.readFileSync('server/views/pages/appointments/create-and-edit/apply-to.njk')
+
+describe('Views - Appointments Management - Apply to', () => {
+  const appointmentId = 1
+  const occurrenceId = 2
+  const weekTomorrow = addDays(new Date(), 8)
+  const applyToOptions = [
+    {
+      applyTo: AppointmentApplyTo.THIS_OCCURRENCE,
+      description: `Just this one - ${formatDate(weekTomorrow, 'EEEE, d MMMM yyyy')} (2 of 3)`,
+    },
+    {
+      applyTo: AppointmentApplyTo.THIS_AND_ALL_FUTURE_OCCURRENCES,
+      description: 'This one and the appointment that comes after it in the series',
+      additionalDescription: `You’re changing appointments 2 to 3`,
+    },
+    {
+      applyTo: AppointmentApplyTo.ALL_FUTURE_OCCURRENCES,
+      description: 'This one and all the appointments in the series that haven’t happened yet',
+      additionalDescription: `You’re changing appointments 1 to 2`,
+    },
+  ] as AppointmentApplyToOption[]
+  let compiledTemplate: Template
+  let viewContext = {
+    session: {
+      appointmentJourney: {} as unknown as AppointmentJourney,
+      editAppointmentJourney: {} as unknown as EditAppointmentJourney,
+    },
+    appointmentId,
+    occurrenceId,
+    property: '',
+    applyToOptions,
+  }
+
+  const njkEnv = registerNunjucks()
+
+  beforeEach(() => {
+    compiledTemplate = nunjucks.compile(view.toString(), njkEnv)
+    viewContext = {
+      session: {
+        appointmentJourney: {
+          mode: AppointmentJourneyMode.EDIT,
+          type: AppointmentType.GROUP,
+          startDate: {
+            day: weekTomorrow.getDate(),
+            month: weekTomorrow.getMonth() + 1,
+            year: weekTomorrow.getFullYear(),
+            date: weekTomorrow,
+          },
+        },
+        editAppointmentJourney: {
+          repeatCount: 3,
+          sequenceNumbers: [1, 2, 3],
+          sequenceNumber: 2,
+        } as EditAppointmentJourney,
+      },
+      appointmentId,
+      occurrenceId,
+      property: 'location',
+      applyToOptions,
+    }
+  })
+
+  it('back link href contains edit property when redirected from property edit page', () => {
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('a.govuk-back-link').attr('href')).toEqual('/appointments/1/occurrence/2/edit/location')
+  })
+
+  it('back link href does not contain edit property when redirected from cancel, add or remove prisoner', () => {
+    viewContext.property = null
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('a.govuk-back-link').attr('href')).toEqual('/appointments/1/occurrence/2')
+  })
+
+  it('prisoner list is not shown when adding one prisoner', () => {
+    viewContext.session.editAppointmentJourney.addPrisoners = [
+      {
+        number: 'A1234BC',
+        name: 'TEST PRISONER',
+        cellLocation: '1-1-1',
+      },
+    ]
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('[data-qa=prisoners-list-table]').length).toEqual(0)
+  })
+
+  it('prisoner list is shown when adding two prisoners', () => {
+    viewContext.session.editAppointmentJourney.addPrisoners = [
+      {
+        number: 'A1234BC',
+        name: 'TEST PRISONER1',
+        cellLocation: '1-1-1',
+      },
+      {
+        number: 'B2345CD',
+        name: 'TEST PRISONER2',
+        cellLocation: '2-2-2',
+      },
+    ]
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('[data-qa=prisoners-list-table]').length).toEqual(1)
+    expect($('[data-qa=prisoner-name-0]').text().trim()).toEqual('Prisoner1, Test')
+    let prisonerDpsLink = $('[data-qa=prisoner-name-0] > a')
+    expect(prisonerDpsLink.attr('href')).toEqual('https://digital-dev.prison.service.justice.gov.uk/prisoner/A1234BC')
+    expect(prisonerDpsLink.attr('target')).toEqual('_blank')
+    expect($('[data-qa=prisoner-number-0]').text().trim()).toEqual('A1234BC')
+    expect($('[data-qa=prisoner-cell-location-0]').text().trim()).toEqual('1-1-1')
+
+    expect($('[data-qa=prisoner-name-1]').text().trim()).toEqual('Prisoner2, Test')
+    prisonerDpsLink = $('[data-qa=prisoner-name-1] > a')
+    expect(prisonerDpsLink.attr('href')).toEqual('https://digital-dev.prison.service.justice.gov.uk/prisoner/B2345CD')
+    expect(prisonerDpsLink.attr('target')).toEqual('_blank')
+    expect($('[data-qa=prisoner-number-1]').text().trim()).toEqual('B2345CD')
+    expect($('[data-qa=prisoner-cell-location-1]').text().trim()).toEqual('2-2-2')
+  })
+
+  it('call to action is confirm and add when adding prisoners', () => {
+    viewContext.session.editAppointmentJourney.addPrisoners = [
+      {
+        number: 'A1234BC',
+        name: 'TEST PRISONER',
+        cellLocation: '1-1-1',
+      },
+    ]
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('button').text().trim()).toEqual('Confirm and add')
+  })
+
+  it('call to action is confirm and add when removing a prisoner', () => {
+    viewContext.session.editAppointmentJourney.removePrisoner = {
+      prisonerNumber: 'A1234BC',
+      bookingId: 1,
+      firstName: 'TEST',
+      lastName: 'PRISONER',
+      prisonCode: 'TPR',
+      cellLocation: '1-1-1',
+    }
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('button').text().trim()).toEqual('Confirm and remove')
+  })
+
+  it('call to action is confirm and save when changing a property', () => {
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('button').text().trim()).toEqual('Confirm and save')
+  })
+})
