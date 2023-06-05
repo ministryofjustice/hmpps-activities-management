7d2c6a90e56d2de0e0ff399c8e18d2029ef5023e
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2023-06-05 16:11:24 +0100

    SAA-781 content changes when entering date for deallocations and adding missing tests.

---
+++ server/routes/deallocate-from-activity/handlers/checkDeallocation.test.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from 'express'
+import { when } from 'jest-when'
+import ActivitiesService from '../../../services/activitiesService'
+import CheckDeallocationRoutes from './checkDeallocation'
+
+jest.mock('../../../services/activitiesService')
+
+const activitiesService = new ActivitiesService(null, null) as jest.Mocked<ActivitiesService>
+
+describe('Route Handlers - Check deallocation', () => {
+  const handler = new CheckDeallocationRoutes(activitiesService)
+
+  let req: Request
+  let res: Response
+
+  const mockActivitiesData = () => {
+    when(activitiesService.getDeallocationReasons).mockResolvedValue([{ code: 'OTHER', description: 'OTHER' }])
+  }
+
+  beforeEach(() => {
+    res = {
+      locals: {
+        user: {},
+      },
+      render: jest.fn(),
+      redirect: jest.fn(),
+    } as unknown as Response
+
+    req = {
+      session: {
+        deallocateJourney: {
+          deallocationDate: '2023-06-05',
+          deallocationReason: 'OTHER',
+        },
+      },
+    } as unknown as Request
+
+    mockActivitiesData()
+  })
+
+  describe('GET', () => {
+    it('should render the expected view', async () => {
+      await handler.GET(req, res)
+      expect(res.render).toHaveBeenCalledWith('pages/deallocate-from-activity/check-answers', {
+        deallocationDate: new Date(2023, 5, 5),
+        deallocationReason: 'OTHER',
+      })
+    })
+  })
+
+  describe('POST', () => {
+    it('redirect with the expected params when deallocated from activity', async () => {
+      req.body = {
+        deallocationReason: 'OTHER',
+        deallocateJourney: {
+          deallocationReason: 'OTHER',
+        },
+      }
+
+      await handler.POST(req, res)
+
+      expect(activitiesService.deallocateFromActivity).toHaveBeenCalledWith(
+        {
+          deallocationDate: '2023-06-05',
+          deallocationReason: 'OTHER',
+        },
+        {},
+      )
+      expect(res.redirect).toHaveBeenCalledWith(`confirmation`)
+    })
+  })
+})
