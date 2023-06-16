05db2136c0d08bebb841db79e021aee06592f31d
Stephen McVeigh <smcveigh941@gmail.com>
2023-06-16 13:28:46 +0100

    Fix create activity links (#336)

--- server/routes/create-an-activity/handlers/startJourney.ts
+++ server/routes/create-an-activity/handlers/startJourney.ts
@@ -3,6 +3,6 @@ import { Request, Response } from 'express'
 export default class StartJourneyRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
     req.session.createJourney = {}
-    res.redirect(`category`)
+    res.redirect(`category${req.query.preserveHistory ? '?preserveHistory=true' : ''}`)
   }
 }
