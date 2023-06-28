4e3e4a023bc31785756139e0984961e26430762c
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-06-28 09:57:29 +0100

    SAA-910: Verified check answers routing for group appointment

--- server/routes/appointments/create-and-edit/handlers/uploadByCsv.ts
+++ server/routes/appointments/create-and-edit/handlers/uploadByCsv.ts
@@ -3,18 +3,24 @@ import { AppointmentType } from '../appointmentJourney'
 
 export default class UploadByCSV {
   GET = async (req: Request, res: Response): Promise<void> => {
+    const { preserveHistory } = req.query
+
     if (req.session.appointmentJourney.type === AppointmentType.BULK) {
-      res.render('pages/appointments/create-and-edit/bulk-appointments/create-bulk-appointment-by-csv')
+      res.render('pages/appointments/create-and-edit/bulk-appointments/create-bulk-appointment-by-csv', {
+        preserveHistory,
+      })
     } else {
-      res.render('pages/appointments/create-and-edit/upload-prisoners-by-csv')
+      res.render('pages/appointments/create-and-edit/upload-prisoners-by-csv', { preserveHistory })
     }
   }
 
   POST = async (req: Request, res: Response): Promise<void> => {
+    const { preserveHistory } = req.query
+
     if (req.session.appointmentJourney.type === AppointmentType.BULK) {
-      res.redirect('upload-bulk-appointment')
+      res.redirect(`upload-bulk-appointment${preserveHistory ? '?preserveHistory=true' : ''}`)
     } else {
-      res.redirect('upload-prisoner-list')
+      res.redirect(`upload-prisoner-list${preserveHistory ? '?preserveHistory=true' : ''}`)
     }
   }
 }
