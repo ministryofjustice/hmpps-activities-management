b6ba99f19aba666227fe55aa4b2360be0a854662
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-06-30 14:55:24 +0100

    SAA-932: Create journey id and add to url

--- server/routes/appointments/index.ts
+++ server/routes/appointments/index.ts
@@ -9,6 +9,7 @@ import bulkAppointmentDetailsRoutes from './bulk-appointment-details'
 import { Services } from '../../services'
 import rolloutMiddleware from '../../middleware/rolloutMiddleware'
 import ServiceName from '../../enum/serviceName'
+import startNewJourney from '../../middleware/startNewJourneyMiddleware'
 
 export default function routes(services: Services): Router {
   const router = Router({ mergeParams: true })
@@ -17,16 +18,28 @@ export default function routes(services: Services): Router {
 
   router.use(rolloutMiddleware(serviceName, services))
 
+  // Appointments tiles route
   router.use('/', appointmentsHomeRoutes())
-  router.use('/create', appointmentsCreateRoutes(services))
+
+  // Search and view appointment routes
   router.use('/search', appointmentSearchRoutes(services))
-
   router.use('/:appointmentId(\\d+)', appointmentDetailsRoutes(services))
-
   router.use('/:appointmentId(\\d+)/occurrence/:occurrenceId(\\d+)', appointmentOccurrenceDetailsRoutes(services))
-  router.use('/:appointmentId(\\d+)/occurrence/:occurrenceId(\\d+)/edit', appointmentsEditRoutes(services))
-
   router.use('/bulk-appointments/:bulkAppointmentId(\\d+)', bulkAppointmentDetailsRoutes(services))
+
+  // Create appointment journey routes
+  router.get('/create/start-individual', startNewJourney('/create/'))
+  router.get('/create/start-group', startNewJourney('/create/'))
+  router.get('/create/start-bulk', startNewJourney('/create/'))
+  router.use('/create/:journeyId', appointmentsCreateRoutes(services))
+
+  // Edit appointment journey routes
+  const editAppointmentBaseUrl = '/:appointmentId(\\d+)/occurrence/:occurrenceId(\\d+)/edit'
+  router.get(`${editAppointmentBaseUrl}/start/cancel`, startNewJourney('/edit/'))
+  router.get(`${editAppointmentBaseUrl}/start/:property`, startNewJourney('/edit/'))
+  router.get(`${editAppointmentBaseUrl}/start/:prisonNumber/remove`, startNewJourney('/edit/'))
+  router.get(`${editAppointmentBaseUrl}/start/prisoners/add`, startNewJourney('/edit/'))
+  router.use(`${editAppointmentBaseUrl}/:journeyId`, appointmentsEditRoutes(services))
 
   return router
 }
