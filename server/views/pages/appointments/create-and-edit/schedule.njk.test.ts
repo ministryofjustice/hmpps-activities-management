6d0561e4ed47f92bc9385defd5fea0c7c36d309d
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-07-06 15:57:17 +0100

    SAA-934: Tests for all appointment details sections

--- server/views/pages/appointments/create-and-edit/schedule.njk.test.ts
+++ server/views/pages/appointments/create-and-edit/schedule.njk.test.ts
@@ -2,13 +2,17 @@ import * as cheerio from 'cheerio'
 import { CheerioAPI } from 'cheerio'
 import nunjucks, { Template } from 'nunjucks'
 import fs from 'fs'
+import { addDays } from 'date-fns'
 import { registerNunjucks } from '../../../../nunjucks/nunjucksSetup'
 import { AppointmentJourney, AppointmentType } from '../../../../routes/appointments/create-and-edit/appointmentJourney'
 import { BulkAppointmentJourney } from '../../../../routes/appointments/create-and-edit/bulkAppointmentJourney'
 import { EventSource, EventType } from '../../../../@types/activities'
+import { formatDate } from '../../../../utils/utils'
 
 let $: CheerioAPI
 const view = fs.readFileSync('server/views/pages/appointments/create-and-edit/schedule.njk')
+
+const tomorrow = addDays(new Date(), 1)
 
 const getAppointmentDetailsValueElement = (heading: string) =>
   $(`[data-qa=appointment-details] > .govuk-summary-list__row > .govuk-summary-list__key`)
@@ -78,7 +82,14 @@ describe('Views - Create Appointment - Schedule', () => {
     compiledTemplate = nunjucks.compile(view.toString(), njkEnv)
     viewContext = {
       session: {
-        appointmentJourney: {} as AppointmentJourney,
+        appointmentJourney: {
+          startDate: {
+            day: tomorrow.getDate(),
+            month: tomorrow.getMonth() + 1,
+            year: tomorrow.getFullYear(),
+            date: formatDate(tomorrow, 'yyyy-MM-dd'),
+          },
+        } as unknown as AppointmentJourney,
         bulkAppointmentJourney: {} as BulkAppointmentJourney,
       },
       prisonerSchedules: [{}],
@@ -95,6 +106,16 @@ describe('Views - Create Appointment - Schedule', () => {
       viewContext.session.appointmentJourney.prisoners = [
         { number: 'A1234BC', name: 'TEST PRISONER', cellLocation: '1-1-1' },
       ]
+      viewContext.session.appointmentJourney.startTime = {
+        hour: 9,
+        minute: 0,
+        date: formatDate(tomorrow.setHours(9, 0), "yyyy-MM-dd'T'HH:mm:ss") as unknown as Date,
+      }
+      viewContext.session.appointmentJourney.endTime = {
+        hour: 10,
+        minute: 30,
+        date: formatDate(tomorrow.setHours(10, 30), "yyyy-MM-dd'T'HH:mm:ss") as unknown as Date,
+      }
       viewContext.prisonerSchedules = viewContext.session.appointmentJourney.prisoners.map(prisoner => ({
         prisoner,
         scheduledEvents: getScheduledEventsForPrisoner(prisoner),
@@ -103,10 +124,29 @@ describe('Views - Create Appointment - Schedule', () => {
       $ = cheerio.load(compiledTemplate.render(viewContext))
     })
 
-    it('should display individual prisoners details', () => {
+    it('should display individual prisoners details with change link', () => {
       expect(getIndividualPrisonerValueElement('prisoner-name').text().trim()).toEqual('Test Prisoner')
       expect(getIndividualPrisonerValueElement('prisoner-number').text().trim()).toEqual('A1234BC')
       expect(getIndividualPrisonerValueElement('prisoner-cell-location').text().trim()).toEqual('1-1-1')
+      expect($('[data-qa=change-prisoner]').attr('href')).toEqual('select-prisoner?preserveHistory=true')
+    })
+
+    it('should not display attendee count', () => {
+      expect(getAppointmentDetailsValueElement('Attendees').length).toEqual(0)
+    })
+
+    it('should not display appointments count', () => {
+      expect(getAppointmentDetailsValueElement('Appointments').length).toEqual(0)
+    })
+
+    it('should display date with change link', () => {
+      expect(getAppointmentDetailsValueElement('Date').text().trim()).toEqual(formatDate(tomorrow, 'EEEE, d MMMM yyyy'))
+      expect($('[data-qa=change-start-date]').attr('href')).toEqual('date-and-time?preserveHistory=true')
+    })
+
+    it('should display time with change link', () => {
+      expect(getAppointmentDetailsValueElement('Time').text().trim()).toEqual('09:00 to 10:30')
+      expect($('[data-qa=change-time]').attr('href')).toEqual('date-and-time?preserveHistory=true')
     })
 
     it('should not display top call to action', () => {
@@ -129,6 +169,16 @@ describe('Views - Create Appointment - Schedule', () => {
         { number: 'I9123JK', name: 'TEST09 PRISONER09', cellLocation: '1-1-9' },
         { number: 'J1234KL', name: 'TEST10 PRISONER10', cellLocation: '1-1-10' },
       ]
+      viewContext.session.appointmentJourney.startTime = {
+        hour: 14,
+        minute: 30,
+        date: formatDate(tomorrow.setHours(14, 30), "yyyy-MM-dd'T'HH:mm:ss") as unknown as Date,
+      }
+      viewContext.session.appointmentJourney.endTime = {
+        hour: 16,
+        minute: 0,
+        date: formatDate(tomorrow.setHours(16, 0), "yyyy-MM-dd'T'HH:mm:ss") as unknown as Date,
+      }
       viewContext.prisonerSchedules = viewContext.session.appointmentJourney.prisoners.map(prisoner => ({
         prisoner,
         scheduledEvents: getScheduledEventsForPrisoner(prisoner),
@@ -139,6 +189,25 @@ describe('Views - Create Appointment - Schedule', () => {
 
     it('should not display individual prisoners details', () => {
       expect(getAppointmentDetailsValueElement('Attendee').length).toBe(0)
+    })
+
+    it('should display attendee count with change link', () => {
+      expect(getAppointmentDetailsValueElement('Attendees').text().trim()).toEqual('10')
+      expect($('[data-qa=change-prisoners]').attr('href')).toEqual('review-prisoners?preserveHistory=true')
+    })
+
+    it('should not display appointments count', () => {
+      expect(getAppointmentDetailsValueElement('Appointments').length).toEqual(0)
+    })
+
+    it('should display date with change link', () => {
+      expect(getAppointmentDetailsValueElement('Date').text().trim()).toEqual(formatDate(tomorrow, 'EEEE, d MMMM yyyy'))
+      expect($('[data-qa=change-start-date]').attr('href')).toEqual('date-and-time?preserveHistory=true')
+    })
+
+    it('should display time with change link', () => {
+      expect(getAppointmentDetailsValueElement('Time').text().trim()).toEqual('14:30 to 16:00')
+      expect($('[data-qa=change-time]').attr('href')).toEqual('date-and-time?preserveHistory=true')
     })
 
     it('should display top call to action for eleven attendees', () => {
@@ -225,6 +294,28 @@ describe('Views - Create Appointment - Schedule', () => {
       }))
 
       $ = cheerio.load(compiledTemplate.render(viewContext))
+    })
+
+    it('should not display individual prisoners details', () => {
+      expect(getAppointmentDetailsValueElement('Attendee').length).toBe(0)
+    })
+
+    it('should not display attendee count', () => {
+      expect(getAppointmentDetailsValueElement('Attendees').length).toEqual(0)
+    })
+
+    it('should display appointments count with change link', () => {
+      expect(getAppointmentDetailsValueElement('Appointments').text().trim()).toEqual('10')
+      expect($('[data-qa=change-prisoners]').attr('href')).toEqual('review-prisoners?preserveHistory=true')
+    })
+
+    it('should display date with change link', () => {
+      expect(getAppointmentDetailsValueElement('Date').text().trim()).toEqual(formatDate(tomorrow, 'EEEE, d MMMM yyyy'))
+      expect($('[data-qa=change-start-date]').attr('href')).toEqual('bulk-appointment-date?preserveHistory=true')
+    })
+
+    it('should not display time', () => {
+      expect(getAppointmentDetailsValueElement('Time').length).toEqual(0)
     })
 
     it('should display top call to action for eleven appointments', () => {
