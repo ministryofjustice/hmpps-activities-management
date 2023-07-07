a83c8ea2599de77381ac2621cacd4154bb16d765
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-07-07 11:04:58 +0100

    Dedicated create start from prisoner profile route

--- server/routes/appointments/create-and-edit/handlers/category.ts
+++ server/routes/appointments/create-and-edit/handlers/category.ts
@@ -20,9 +20,7 @@ export default class CategoryRoutes {
 
     let backLinkHref = 'review-prisoners'
     if (appointmentJourney.type === AppointmentType.INDIVIDUAL) {
-      if (appointmentJourney.fromPrisonNumberProfile) {
-        backLinkHref = `${config.dpsUrl}/prisoner/${appointmentJourney.fromPrisonNumberProfile}`
-      } else if (appointmentJourney.prisoners?.length > 0) {
+      if (appointmentJourney.prisoners?.length > 0) {
         backLinkHref = `select-prisoner?query=${appointmentJourney.prisoners[0].number}`
       } else {
         backLinkHref = 'select-prisoner'
