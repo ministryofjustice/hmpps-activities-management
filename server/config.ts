f1a067af174e6b3b400750fda35ebca9b489a81d
Stephen Mc Veigh <smcveigh941@gmail.com>
2023-07-12 17:43:11 +0100

    Hide spike routes in production

--- server/config.ts
+++ server/config.ts
@@ -134,4 +134,5 @@ export default {
       roles: [Roles.ACTIVITY_HUB],
     },
   ] as RouteAuth[],
+  spikesFeatureToggle: Boolean(get('SPIKES_FEATURE_TOGGLE', false)),
 }
--- server/routes/spikes/index.ts
+++ server/routes/spikes/index.ts
@@ -1,7 +1,9 @@
 import { RequestHandler, Router } from 'express'
+import createHttpError from 'http-errors'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import { Services } from '../../services'
 import CalendarSpikeRoutes from './handlers/calendarSpike'
+import config from '../../config'
 
 export default function Index({ activitiesService }: Services): Router {
   const router = Router()
@@ -9,6 +11,11 @@ export default function Index({ activitiesService }: Services): Router {
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   const calendarSpikeHandler = new CalendarSpikeRoutes(activitiesService)
+
+  // Spike routes are only accessible when running locally or when feature toggle is provided
+  router.use((req, res, next) =>
+    process.env.NODE_ENV === 'production' && !config.spikesFeatureToggle ? next(createHttpError.NotFound()) : next(),
+  )
 
   get('/calendar-spike', calendarSpikeHandler.GET)
   get('/calendar-spike/search', calendarSpikeHandler.PRISONER_SEARCH)
