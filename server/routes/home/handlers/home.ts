f9908abb7e8bb32efe7acde35009faec0c64194f
Stephen McVeigh <smcveigh941@gmail.com>
2023-07-25 12:06:07 +0100

    Render not rolled out template if neither service is rolled out (#447)

--- server/routes/home/handlers/home.ts
+++ server/routes/home/handlers/home.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
-import config from '../../../config'
 
 export default class HomeRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
     const { isActivitiesRolledOut, isAppointmentsRolledOut } = res.locals.user
 
     if (!isActivitiesRolledOut && !isAppointmentsRolledOut) {
-      return res.redirect(config.dpsUrl)
+      return res.render('pages/not-rolled-out', {
+        serviceName: 'Activities and Appointments',
+      })
     }
     return res.render('pages/home/index')
   }
