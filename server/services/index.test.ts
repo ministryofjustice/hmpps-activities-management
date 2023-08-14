7ab255c842a1e4b5aef0fd198796d250e3151798
gerry.allan <gerry.allan@digital.justice.gov.uk>
2023-08-14 16:51:32 +0100

    SAA-864 - log key events - appinsights

--- server/services/index.test.ts
+++ server/services/index.test.ts
@@ -9,7 +9,7 @@ describe('Services', () => {
   test('The correct services are instantiated', () => {
     const servicesList = services()
 
-    expect(Object.values(servicesList).length).toBe(5)
+    expect(Object.values(servicesList).length).toBe(6)
     expect(servicesList.userService).toBeInstanceOf(UserService)
     expect(servicesList.prisonService).toBeInstanceOf(PrisonService)
     expect(servicesList.ukBankHolidayService).toBeInstanceOf(BankHolidayService)
