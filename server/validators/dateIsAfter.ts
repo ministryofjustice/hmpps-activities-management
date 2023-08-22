ac20f59b354ca6ea9739e1728d2b1ebbde3b0c89
gerryallan-moj <123087637+gerryallan-moj@users.noreply.github.com>
2023-08-22 09:13:04 +0100

    SAA-1125 - start date validation on create activity journey (#501)

--- server/validators/dateIsAfter.ts
+++ server/validators/dateIsAfter.ts
@@ -1,10 +1,10 @@
 import { registerDecorator, ValidationOptions } from 'class-validator'
-import { isValid, startOfDay } from 'date-fns'
+import { isValid } from 'date-fns'
 import SimpleDate from '../commonValidationTypes/simpleDate'
 
 export default function DateIsAfter(dateToCompare: Date, validationOptions?: ValidationOptions) {
   const dateIsAfter = (date: SimpleDate) =>
-    date !== undefined && isValid(date.toRichDate()) ? startOfDay(date.toRichDate()) > startOfDay(dateToCompare) : true
+    date !== undefined && isValid(date.toRichDate()) ? date.toIsoString() > dateToCompare.toISOString() : true
 
   return (object: unknown, propertyName: string) => {
     registerDecorator({
