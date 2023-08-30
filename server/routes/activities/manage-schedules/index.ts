7b7043958b9a053cfbfea4e79fac325436d81b59
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2023-08-30 13:46:41 +0100

    SAA-1089: Confirm capacity page updates

--- server/routes/activities/manage-schedules/index.ts
+++ server/routes/activities/manage-schedules/index.ts
@@ -3,7 +3,6 @@ import asyncMiddleware from '../../../middleware/asyncMiddleware'
 import { Services } from '../../../services'
 import ActivitiesRoutes from './handlers/activities'
 import ActivityRoutes from './handlers/activity'
-import ConfirmCapacityRoutes from './handlers/confirmCapacity'
 import CheckPayRoutes from './handlers/checkPay'
 
 export default function Index(services: Services): Router {
@@ -12,9 +11,6 @@ export default function Index(services: Services): Router {
   router.get('/activities', asyncMiddleware(activitiesListRouteHandler.GET))
   const activityRouteHandler = new ActivityRoutes(services.activitiesService, services.prisonService)
   router.get('/activities/:activityId', asyncMiddleware(activityRouteHandler.GET))
-  const confirmCapacityRouteHandler = new ConfirmCapacityRoutes(services.activitiesService)
-  router.get('/confirm-capacity', asyncMiddleware(confirmCapacityRouteHandler.GET))
-  router.post('/confirm-capacity', asyncMiddleware(confirmCapacityRouteHandler.POST))
   const checkPayRouteHandler = new CheckPayRoutes(services.activitiesService, services.prisonService)
   router.get('/check-pay', asyncMiddleware(checkPayRouteHandler.GET))
   router.post('/check-pay', asyncMiddleware(checkPayRouteHandler.POST))
