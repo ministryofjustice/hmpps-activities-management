a84ac88eb092ab071a3f688ecd3588f28df9e59b
Stephen Mc Veigh <smcveigh941@gmail.com>
2023-09-06 16:26:37 +0100

    Fetch the user each request, because the user object may have changed in the DPS service

--- server/middleware/populateCurrentUser.ts
+++ server/middleware/populateCurrentUser.ts
@@ -5,9 +5,7 @@ import UserService from '../services/userService'
 export default function populateCurrentUser(userService: UserService): RequestHandler {
   return async (req, res, next) => {
     try {
-      if (!req.session.user) {
-        req.session.user = await userService.getUser(res.locals.user)
-      }
+      req.session.user = await userService.getUser(res.locals.user)
       res.locals.user = req.session.user
       next()
     } catch (error) {
