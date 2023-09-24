e939ec8a0e354cddadd4e58b655818341f85f258
Simon Oldham <simon.oldham@digital.justice.gov.uk>
2023-09-24 20:54:16 +0100

    Add allocation timing metric

--- server/routes/activities/allocate-to-activity/handlers/confirmation.ts
+++ server/routes/activities/allocate-to-activity/handlers/confirmation.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from 'express'
+import { trackEvent } from '../../../../utils/eventTrackingAppInsights'
 
 export default class ConfirmationRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
-    const { inmate, activity } = req.session.allocateJourney
+    const { inmate, activity, startDate } = req.session.allocateJourney
 
     res.render('pages/activities/allocate-to-activity/confirmation', {
       activityId: activity.activityId,
       prisonerName: inmate.prisonerName,
       prisonerNumber: inmate.prisonerNumber,
       activityName: activity.name,
+    })
+
+    const properties = {
+      user: res.locals.user.username,
+      prisonCode: res.locals.user.activeCaseLoadId,
+      prisonerNumber: inmate.prisonerNumber,
+      activityId: activity.activityId?.toString(),
+      startDate: startDate?.toString(),
+      requestDate: Date.now().toString(),
+      // TODO source
+    }
+
+    const eventMetrics = {
+      journeyTimeSec: (Date.now() - req.session.journeyStartTime) / 1000,
+      // TODO  applicationWaitBeforeAllocationTimeDays
+    }
+
+    trackEvent({
+      eventName: 'SAA-Allocation-Created',
+      properties,
+      eventMetrics,
     })
 
     req.session.allocateJourney = null
