d8ba1b206b2e3379a6f3185a68817b451c50656f
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2023-09-25 17:10:46 +0100

    SAA-1220: Lowered micro-frontends client timeout from 50 to 5 seconds (#564)

--- server/config.ts
+++ server/config.ts
@@ -80,8 +80,8 @@ export default {
     frontendComponents: {
       url: get('FRONTEND_COMPONENT_API_URL', 'http://localhost:8082', requiredInProduction),
       timeout: {
-        response: Number(get('FRONTEND_COMPONENT_API_TIMEOUT_RESPONSE', 50000)),
-        deadline: Number(get('FRONTEND_COMPONENT_API_TIMEOUT_DEADLINE', 50000)),
+        response: Number(get('FRONTEND_COMPONENT_API_TIMEOUT_RESPONSE', 5000)),
+        deadline: Number(get('FRONTEND_COMPONENT_API_TIMEOUT_DEADLINE', 5000)),
       },
       agent: new AgentConfig(Number(get('FRONTEND_COMPONENT_API_TIMEOUT_RESPONSE', 5000))),
     },
