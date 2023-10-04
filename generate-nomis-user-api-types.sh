806683899b688ec874d54a17fb1daa85f44a7282
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-10-04 14:58:07 +0100

    Update nomis-user-roles-api dev url in  generate-nomis-user-api-types.sh

--- generate-nomis-user-api-types.sh
+++ generate-nomis-user-api-types.sh
@@ -1,1 +1,1 @@
-npx openapi-typescript https://nomis-user-dev.aks-dev-1.studio-hosting.service.justice.gov.uk/v3/api-docs > server/@types/nomisUserApiImport/index.d.ts
+npx openapi-typescript https://nomis-user-roles-api-dev.prison.service.justice.gov.uk/v3/api-docs > server/@types/nomisUserApiImport/index.d.ts
