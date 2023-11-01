8da8835f53d88f0c4c520b83a6b8561fcd990443
Stephen Mc Veigh <smcveigh941@gmail.com>
2023-11-01 09:51:46 +0000

    SAA-1350: Allow form submissions to prison estate domains

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -26,6 +26,7 @@ export default function setUpWebSecurity(): Router {
   const styleSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
   const imgSrc = ["'self'", 'data:']
   const fontSrc = ["'self'"]
+  const formAction = ["'self' *.prison.service.justice.gov.uk"]
 
   scriptSrc.push(config.apis.frontendComponents.url)
   styleSrc.push(config.apis.frontendComponents.url)
@@ -42,6 +43,7 @@ export default function setUpWebSecurity(): Router {
           styleSrc,
           imgSrc,
           fontSrc,
+          formAction,
         },
       },
       referrerPolicy: { policy: 'same-origin' },
