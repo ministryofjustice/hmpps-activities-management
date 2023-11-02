a2b516cc55a21d5044a9b56306fb2811fa792387
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2023-11-02 11:29:27 +0000

    SAA-1348: Adds custom dimensions (username, activeCaseLoadId) to app insights request data (#639)

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,5 +1,11 @@
-import { setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
+import { Contracts, setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
+import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
 import applicationVersion from '../applicationVersion'
+
+export type ContextObject = {
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  [name: string]: any
+}
 
 function defaultName(): string {
   const {
@@ -26,7 +32,31 @@ export function buildAppInsightsClient(name = defaultName()): TelemetryClient {
   if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING && defaultClient) {
     defaultClient.context.tags['ai.cloud.role'] = name
     defaultClient.context.tags['ai.application.ver'] = version()
+    defaultClient.addTelemetryProcessor(addUserDataToRequests)
     return defaultClient
   }
   return null
+}
+
+/**
+ * Adds extra data in the requests logged for the service, to identify unique username and active caseload.
+ * @param envelope
+ * @param contextObjects
+ */
+export function addUserDataToRequests(envelope: EnvelopeTelemetry, contextObjects: ContextObject) {
+  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
+  if (isRequest) {
+    const { caseLoadId } = contextObjects?.['http.ServerRequest']?.res?.locals?.user?.activeCaseLoad || {}
+    const { username } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
+    if (username) {
+      const { properties } = envelope.data.baseData
+      // eslint-disable-next-line no-param-reassign
+      envelope.data.baseData.properties = {
+        username,
+        activeCaseLoadId: caseLoadId,
+        ...properties,
+      }
+    }
+  }
+  return true
 }
