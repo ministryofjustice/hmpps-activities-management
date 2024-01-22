bcf13e5d394dabf1ef8ec37d677724d43d5f52ff
David Thomas <david.thomas1@digital.justice.gov.uk>
2024-01-22 16:21:59 +0000

    SAA-1119: Update valiation error messages

--- server/routes/activities/record-attendance/handlers/uncancel-session/confirmation.test.ts
+++ server/routes/activities/record-attendance/handlers/uncancel-session/confirmation.test.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
-import ConfirmationRoutes from './confirmation'
+import { plainToInstance } from 'class-transformer'
+import { validate } from 'class-validator'
+import ConfirmationRoutes, { UncancelConfirmForm } from './confirmation'
 import ActivitiesService from '../../../../../services/activitiesService'
+import { associateErrorsWithProperty } from '../../../../../utils/utils'
 
 jest.mock('../../../../../services/activitiesService')
 
@@ -76,6 +79,26 @@ describe('Route Handlers - Uncancel Session Confirmation', () => {
 
       expect(activitiesService.uncancelScheduledActivity).toHaveBeenCalledTimes(0)
       expect(res.redirect).toHaveBeenCalledWith('/activities/attendance/activities/1/attendance-list')
+    })
+  })
+
+  describe('Validation', () => {
+    it('validation fails when confirmation value not selected', async () => {
+      const body = {}
+
+      const requestObject = plainToInstance(UncancelConfirmForm, body)
+      const errors = await validate(requestObject).then(errs => errs.flatMap(associateErrorsWithProperty))
+      expect(errors).toEqual([{ property: 'confirm', error: 'Confirm if you want to uncancel the session or not' }])
+    })
+
+    it('validation should pass when valid confirmation option selected', async () => {
+      const body = {
+        confirm: 'yes',
+      }
+
+      const requestObject = plainToInstance(UncancelConfirmForm, body)
+      const errors = await validate(requestObject).then(errs => errs.flatMap(associateErrorsWithProperty))
+      expect(errors).toHaveLength(0)
     })
   })
 })
