f325d6255e7943db4f383c6e4125892fae3d9e68
Lance Munslow <lance@munslow.net>
2024-03-07 08:37:26 +0000

    SAA-1614: Fix missing in cell location in appointments dashboard search results (#770)

--- integration_tests/pages/appointments/appointmentsManagementPage.ts
+++ integration_tests/pages/appointments/appointmentsManagementPage.ts
@@ -6,4 +6,6 @@ export default class AppointmentsManagementPage extends Page {
   }
 
   createGroupAppointmentCard = (): Cypress.Chainable => cy.get('[data-qa=create-group-appointment-card]')
+
+  searchAppointmentsCard = (): Cypress.Chainable => cy.get('[data-qa=search-appointments-card]')
 }
