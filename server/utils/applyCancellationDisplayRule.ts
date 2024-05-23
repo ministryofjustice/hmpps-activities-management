20b97b688bda7dfb12ac3cee3c4158409b01386a
dafhoustonmoj <dafydd.houston@digital.justice.gov.uk>
2024-05-23 10:25:10 +0100

    SAA-1143 Typo in variable name

--- server/utils/applyCancellationDisplayRule.ts
+++ server/utils/applyCancellationDisplayRule.ts
@@ -5,31 +5,31 @@ import { toDate } from './utils'
 
 export default function applyCancellationDisplayRule(app: ScheduledEvent): boolean {
   let showAppointment = true
-  const beginingOfToday = new Date().setHours(0, 0, 0, 0)
+  const beginningOfToday = new Date().setHours(0, 0, 0, 0)
   if (app.cancelled) {
     if (
       app.appointmentSeriesFrequency === AppointmentFrequency.DAILY &&
-      toDate(app.appointmentSeriesCancellationStartDate) < subDays(beginingOfToday, 2)
+      toDate(app.appointmentSeriesCancellationStartDate) < subDays(beginningOfToday, 2)
     ) {
       showAppointment = false
     } else if (
       app.appointmentSeriesFrequency === AppointmentFrequency.WEEKDAY &&
-      toDate(app.appointmentSeriesCancellationStartDate) < subDays(beginingOfToday, 4)
+      toDate(app.appointmentSeriesCancellationStartDate) < subDays(beginningOfToday, 4)
     ) {
       showAppointment = false
     } else if (
       app.appointmentSeriesFrequency === AppointmentFrequency.WEEKLY &&
-      toDate(app.appointmentSeriesCancellationStartDate) < subWeeks(beginingOfToday, 2)
+      toDate(app.appointmentSeriesCancellationStartDate) < subWeeks(beginningOfToday, 2)
     ) {
       showAppointment = false
     } else if (
       app.appointmentSeriesFrequency === AppointmentFrequency.FORTNIGHTLY &&
-      toDate(app.appointmentSeriesCancellationStartDate) < subWeeks(beginingOfToday, 4)
+      toDate(app.appointmentSeriesCancellationStartDate) < subWeeks(beginningOfToday, 4)
     ) {
       showAppointment = false
     } else if (
       app.appointmentSeriesFrequency === AppointmentFrequency.MONTHLY &&
-      toDate(app.appointmentSeriesCancellationStartDate) < subMonths(beginingOfToday, 2)
+      toDate(app.appointmentSeriesCancellationStartDate) < subMonths(beginningOfToday, 2)
     ) {
       showAppointment = false
     }
