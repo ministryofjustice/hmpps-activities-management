4937b259a4944e2790c85e7f3fbfddf5012af42e
tim <wright.tim.j@gmail.com>
2024-06-06 10:19:17 +0100

    SAA-1735 missing the word by

--- integration_tests/e2e/recordAttendance.cy.ts
+++ integration_tests/e2e/recordAttendance.cy.ts
@@ -91,7 +91,7 @@ context('Record attendance', () => {
     attendanceListPage.checkAttendanceStatus('Aisho, Egurztof', 'Pay')
     attendanceListPage.assertNotificationContents(
       'Session cancelled',
-      'This activity session has been cancelled J. Smith on Thursday, 2 February 2023 for the following reason:',
+      'This activity session has been cancelled by J. Smith on Thursday, 2 February 2023 for the following reason:',
     )
 
     attendanceListPage.getLinkByText('Uncancel this session').click()
