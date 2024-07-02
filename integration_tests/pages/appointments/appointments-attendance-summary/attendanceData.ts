decc2f4cc60cd33095b7b8292706f160bec00878
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2024-07-02 13:28:27 +0100

    SAA-1815: Remove comment

--- integration_tests/pages/appointments/appointments-attendance-summary/attendanceData.ts
+++ integration_tests/pages/appointments/appointments-attendance-summary/attendanceData.ts
@@ -13,8 +13,6 @@ export default class AttendanceData extends Page {
 
   table = (): Cypress.Chainable => cy.get('[data-qa="appointment-attendance-data"]')
 
-  // searchBar = (): Cypress.Chainable => cy.get('#search-input')
-
   searchbarSubmit = (): Cypress.Chainable => cy.get('[data-qa="searchbar-submit"]')
 
   appointmentName = (appId: number): Cypress.Chainable => cy.get(`[data-qa="appointmentName-${appId}"]`)
