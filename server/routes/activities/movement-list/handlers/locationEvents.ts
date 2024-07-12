3640a807a42bc42dd94626ea07978bee241c7ae7
tim <wright.tim.j@gmail.com>
2024-07-12 14:40:53 +0100

    SAA-1860 remove comment

--- server/routes/activities/movement-list/handlers/locationEvents.ts
+++ server/routes/activities/movement-list/handlers/locationEvents.ts
@@ -103,7 +103,6 @@ export default class LocationEventsRoutes {
                         .map(e => e.eventId)
                         .includes(ce.eventId),
                   )
-                  // Exclude any adjudications
                   .filter(
                     ce =>
                       !events
