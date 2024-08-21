27469129c448aa8de6025bffd8002db2a2c53ed3
dafhoustonmoj <dafydd.houston@digital.justice.gov.uk>
2024-08-21 14:41:07 +0100

    SAA-1940 add logic to post custom slots after the session times have been entered

--- server/routes/activities/create-an-activity/handlers/sessionTimes.ts
+++ server/routes/activities/create-an-activity/handlers/sessionTimes.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from 'express'
 import { ValidateNested } from 'class-validator'
-import { Transform, plainToInstance } from 'class-transformer'
+import { plainToInstance, Transform } from 'class-transformer'
 import ActivitiesService from '../../../../services/activitiesService'
-import getApplicableDaysAndSlotsInRegime from '../../../../utils/helpers/applicableRegimeTimeUtil'
+import getApplicableDaysAndSlotsInRegime, {
+  DaysAndSlotsInRegime,
+} from '../../../../utils/helpers/applicableRegimeTimeUtil'
 import { Slots } from '../journey'
-import { journeySlotsToCustomSlots } from '../../../../utils/helpers/activityTimeSlotMappers'
 import { Slot } from '../../../../@types/activitiesAPI/types'
 import TimeSlot from '../../../../enum/timeSlot'
 import SimpleTime from '../../../../commonValidationTypes/simpleTime'
+import { createCustomSlots } from '../../../../utils/helpers/activityTimeSlotMappers'
 
+type SessionSlot = {
+  dayOfWeek: string
+  timeSlot: TimeSlot
+  start: string
+  finish: string
+  isFirst: boolean
+}
 export class SessionTimes {
+  // validate start before end & sessions (do not overlap???)
   @Transform(({ value }) =>
     Object.keys(value).reduce((acc, k) => acc.set(k, plainToInstance(SimpleTime, value[k])), new Map()),
   )
@@ -23,6 +33,41 @@ export class SessionTimes {
   endTime: Map<string, SimpleTime>
 }
 
+function createSessionSlots(applicableRegimeTimesForActivity: DaysAndSlotsInRegime[]): SessionSlot[] {
+  const sessionSlots: SessionSlot[] = []
+  applicableRegimeTimesForActivity.forEach(day => {
+    if (day.amStart) {
+      sessionSlots.push({
+        dayOfWeek: day.dayOfWeek,
+        timeSlot: TimeSlot.AM,
+        start: day.amStart,
+        finish: day.amFinish,
+        isFirst: true,
+      })
+    }
+    if (day.pmStart) {
+      sessionSlots.push({
+        dayOfWeek: day.dayOfWeek,
+        timeSlot: TimeSlot.PM,
+        start: day.pmStart,
+        finish: day.pmFinish,
+        isFirst: !day.amStart,
+      })
+    }
+    if (day.edStart) {
+      sessionSlots.push({
+        dayOfWeek: day.dayOfWeek,
+        timeSlot: TimeSlot.ED,
+        start: day.edStart,
+        finish: day.edFinish,
+        isFirst: !day.amStart && !day.pmStart,
+      })
+    }
+  })
+
+  return sessionSlots
+}
+
 export default class SessionTimesRoutes {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
@@ -32,47 +77,16 @@ export default class SessionTimesRoutes {
 
     // TODO: the week will have to be passed through from previous pages once we do split regimes, rather than hardcoded as ['1']
     const slots: Slots = req.session.createJourney.slots['1']
-    // TODO: implement real custom slots
-    const customSlots: Slot[] = journeySlotsToCustomSlots(slots)
 
-    const applicableRegimeTimesForActivity = getApplicableDaysAndSlotsInRegime(regimeTimes, slots)
+    const applicableRegimeTimesForActivity: DaysAndSlotsInRegime[] = getApplicableDaysAndSlotsInRegime(
+      regimeTimes,
+      slots,
+    )
 
-    const sessionSlots: { dayOfWeek: string; timeSlot: TimeSlot; start: string; finish: string; isFirst: boolean }[] =
-      []
-
-    applicableRegimeTimesForActivity.forEach(day => {
-      if (day.amStart) {
-        sessionSlots.push({
-          dayOfWeek: day.dayOfWeek,
-          timeSlot: TimeSlot.AM,
-          start: day.amStart,
-          finish: day.amFinish,
-          isFirst: true,
-        })
-      }
-      if (day.pmStart) {
-        sessionSlots.push({
-          dayOfWeek: day.dayOfWeek,
-          timeSlot: TimeSlot.PM,
-          start: day.pmStart,
-          finish: day.pmFinish,
-          isFirst: !day.amStart,
-        })
-      }
-      if (day.edStart) {
-        sessionSlots.push({
-          dayOfWeek: day.dayOfWeek,
-          timeSlot: TimeSlot.ED,
-          start: day.edStart,
-          finish: day.edFinish,
-          isFirst: !day.amStart && !day.pmStart,
-        })
-      }
-    })
+    const sessionSlots: SessionSlot[] = createSessionSlots(applicableRegimeTimesForActivity)
 
     res.render(`pages/activities/create-an-activity/session-times`, {
       regimeTimes: applicableRegimeTimesForActivity,
-      customSlots,
       sessionSlots,
     })
   }
@@ -80,35 +94,9 @@ export default class SessionTimesRoutes {
   POST = async (req: Request, res: Response): Promise<void> => {
     const { startTime, endTime }: SessionTimes = req.body
 
-    const startTimeObj = Array.from(startTime.keys()).reduce((acc, value) => {
-      acc[value] = startTime.get(value)
-      return value
-    })
+    const customSlots: Slot[] = createCustomSlots(startTime, endTime)
+    req.session.createJourney.customSlots = customSlots
 
-    const endTimeObj = Array.from(endTime.keys()).reduce((acc, value) => {
-      acc[value] = endTime.get(value)
-      return value
-    })
-
-    req.body.startTime = startTimeObj
-    req.body.endTime = endTimeObj
-
-    // const customSlots = req.session.createJourney.customSlots.map(customSlot => ({
-    //   customStartTime: startTimeObj,
-    //   customEndTime: endTimeObj,
-    //   daysOfWeek: customSlot.daysOfWeek,
-    //   friday: customSlot.friday,
-    //   monday: customSlot.monday,
-    //   saturday: customSlot.saturday,
-    //   sunday: customSlot.sunday,
-    //   thursday: customSlot.thursday,
-    //   timeSlot: customSlot.timeSlot,
-    //   tuesday: customSlot.tuesday,
-    //   wednesday: customSlot.wednesday,
-    //   weekNumber: 1,
-    // }))
-    // // const { customSlots } = req.body
-    // req.session.createJourney.customSlots = customSlots
     res.redirectOrReturn('location')
   }
 }
