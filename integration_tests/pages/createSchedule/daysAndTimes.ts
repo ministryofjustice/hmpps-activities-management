1ce0286f64c48b38ecfc79ba34b110a5c9ecef06
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2024-09-02 10:50:10 +0100

    SAA-1913: Integration tests & fixes for Custom times edit path (#1047)
    * SAA-1913: Int test setup
    
    * SAA-1913: Fix button issue

--- integration_tests/pages/createSchedule/daysAndTimes.ts
+++ integration_tests/pages/createSchedule/daysAndTimes.ts
@@ -5,6 +5,8 @@ export default class DaysAndTimesPage extends Page {
     super('create-schedule-days-and-times-page')
   }
 
+  checkboxes = () => cy.get('[data-qa="day-session-checkboxes"]')
+
   selectDayTimeCheckboxes = (checkboxes: [string, string[]][]) =>
     checkboxes.forEach(dayCheckbox => {
       const input = this.getInputByLabel(dayCheckbox[0]).click()
@@ -16,4 +18,8 @@ export default class DaysAndTimesPage extends Page {
         .then(conditionalContainerId => cy.get(`#${conditionalContainerId}`).as('conditionalContainer'))
       dayCheckbox[1].forEach(timeCheckbox => cy.get('@conditionalContainer').contains('label', timeCheckbox).click())
     })
+
+  uncheckAllCheckboxes = () => cy.get('[type="checkbox"]').uncheck({ force: true })
+
+  updateButton = () => cy.get('button').contains('Update days and sessions').click()
 }
