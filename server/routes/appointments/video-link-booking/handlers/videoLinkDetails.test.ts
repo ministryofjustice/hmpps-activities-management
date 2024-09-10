4d9f4581b89abb7a1b5641d96bce45ee7468e0ed
Stephen McVeigh <smcveigh941@gmail.com>
2024-09-10 09:11:10 +0100

    BAVL-334: Generate movement slip for a video link booking (#1064)

--- server/routes/appointments/video-link-booking/handlers/videoLinkDetails.test.ts
+++ server/routes/appointments/video-link-booking/handlers/videoLinkDetails.test.ts
@@ -6,8 +6,12 @@ import UserService from '../../../../services/userService'
 import { VideoLinkBooking } from '../../../../@types/bookAVideoLinkApi/types'
 import { Prisoner } from '../../../../@types/prisonerOffenderSearchImport/types'
 import { UserDetails } from '../../../../@types/manageUsersApiImport/types'
+import ActivitiesService from '../../../../services/activitiesService'
+import { LocationLenient } from '../../../../@types/prisonApiImportCustom'
+import { AppointmentSearchResult } from '../../../../@types/activitiesAPI/types'
 
 jest.mock('../../../../services/bookAVideoLinkService')
+jest.mock('../../../../services/activitiesService')
 jest.mock('../../../../services/prisonService')
 jest.mock('../../../../services/userService')
 
@@ -15,6 +19,7 @@ describe('VideoLinkDetailsRoutes', () => {
   let req: Partial<Request>
   let res: Partial<Response>
   let bookAVideoLinkService: jest.Mocked<BookAVideoLinkService>
+  let activitiesService: jest.Mocked<ActivitiesService>
   let prisonService: jest.Mocked<PrisonService>
   let userService: jest.Mocked<UserService>
   let videoLinkDetailsRoutes: VideoLinkDetailsRoutes
@@ -28,9 +33,15 @@ describe('VideoLinkDetailsRoutes', () => {
       render: jest.fn(),
     } as unknown as Response
     bookAVideoLinkService = new BookAVideoLinkService(null) as jest.Mocked<BookAVideoLinkService>
+    activitiesService = new ActivitiesService(null) as jest.Mocked<ActivitiesService>
     prisonService = new PrisonService(null, null, null) as jest.Mocked<PrisonService>
     userService = new UserService(null, null, null) as jest.Mocked<UserService>
-    videoLinkDetailsRoutes = new VideoLinkDetailsRoutes(bookAVideoLinkService, prisonService, userService)
+    videoLinkDetailsRoutes = new VideoLinkDetailsRoutes(
+      bookAVideoLinkService,
+      activitiesService,
+      prisonService,
+      userService,
+    )
   })
 
   describe('GET', () => {
@@ -82,6 +93,7 @@ describe('VideoLinkDetailsRoutes', () => {
         dateOfBirth: '1980-01-01',
         prisonId: 'PRISON1',
       } as Prisoner)
+      prisonService.getInternalLocationByKey.mockResolvedValue({ locationId: 10001 } as LocationLenient)
       bookAVideoLinkService.getAppointmentLocations.mockResolvedValue([
         { key: 'Room1', description: 'Room 1', enabled: true },
       ])
@@ -92,6 +104,14 @@ describe('VideoLinkDetailsRoutes', () => {
         ]) as Map<string, UserDetails>,
       )
       bookAVideoLinkService.bookingIsAmendable.mockReturnValue(true)
+      activitiesService.searchAppointments.mockResolvedValue([
+        {
+          appointmentId: 1,
+          internalLocation: { id: 10001 },
+          startTime: '10:30',
+          endTime: '11:00',
+        } as AppointmentSearchResult,
+      ])
 
       await videoLinkDetailsRoutes.GET(req as Request, res as Response)
 
@@ -99,6 +119,7 @@ describe('VideoLinkDetailsRoutes', () => {
         'pages/appointments/video-link-booking/details',
         expect.objectContaining({
           videoBooking,
+          mainAppointmentId: 1,
           preAppointment: videoBooking.prisonAppointments[0],
           mainAppointment: videoBooking.prisonAppointments[1],
           postAppointment: videoBooking.prisonAppointments[2],
