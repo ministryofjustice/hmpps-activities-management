42e9007d684cf5f0a5efe73ee436d0d7ff2794b2
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2024-11-07 17:14:17 +0000

    SAA-2164

--- integration_tests/e2e/appointments/editAppointments.cy.ts
+++ integration_tests/e2e/appointments/editAppointments.cy.ts
@@ -3,6 +3,17 @@ import getRepeatGroupAppointmentSeriesDetails from '../../fixtures/activitiesApi
 import getRepeatGroupAppointment2Details from '../../fixtures/activitiesApi/getRepeatGroupAppointment2Details.json'
 import getAppointmentLocations from '../../fixtures/prisonApi/getMdiAppointmentLocations.json'
 import getScheduledEvents from '../../fixtures/activitiesApi/getScheduledEventsMdi20230202.json'
+import getPrisonPrisonersG0995GW from '../../fixtures/prisonerSearchApi/getPrisonPrisoners-MDI-G0995GW.json'
+import getPrisonPrisonersG6123VU from '../../fixtures/prisonerSearchApi/getPrisonPrisoners-MDI-G6123VU.json'
+import getPrisonerG0995GW from '../../fixtures/prisonerSearchApi/getPrisoner-MDI-G0995GW.json'
+import getPrisonerG6123VU from '../../fixtures/prisonerSearchApi/getPrisoner-MDI-G6123VU.json'
+import getOffenderAlertsG0995GW from '../../fixtures/activitiesApi/getOffenderAlertsG0995GW.json'
+import getOffenderAlertsG0995GWG6123VU from '../../fixtures/activitiesApi/getOffenderAlertsG0995GWG6123VU.json'
+import getNonAssociationsBetweenG0995GWA1350DZ from '../../fixtures/nonAssociationsApi/getNonAssociationsBetweenG0995GWA1350DZ.json'
+import getNonAssociationsBetweenG0995GWG6123VU from '../../fixtures/nonAssociationsApi/getNonAssociationsBetweenG0995GWG6123VU.json'
+import getPrisonPrisonersG0995GWA1351DZ from '../../fixtures/prisonerSearchApi/postPrisonerNumbers-A1350DZ-G0995GW.json'
+import getPrisonPrisonersG0995GWG6123VU from '../../fixtures/prisonerSearchApi/postPrisonerNumbers-G6123VU-G0995GW.json'
+
 import Page from '../../pages/page'
 import AppointmentDetailsPage from '../../pages/appointments/appointment/appointmentDetailsPage'
 import { formatDate } from '../../../server/utils/utils'
@@ -10,6 +21,11 @@ import LocationPage from '../../pages/appointments/create-and-edit/locationPage'
 import DateAndTimePage from '../../pages/appointments/create-and-edit/dateAndTimePage'
 import ExtraInformationPage from '../../pages/appointments/create-and-edit/extraInformationPage'
 import SchedulePage from '../../pages/appointments/create-and-edit/schedulePage'
+import HowToAddPrisonersPage from '../../pages/appointments/create-and-edit/howToAddPrisonersPage'
+import SelectPrisonerPage from '../../pages/appointments/create-and-edit/selectPrisonerPage'
+import ReviewPrisonersPage from '../../pages/appointments/create-and-edit/reviewPrisonersPage'
+import ReviewPrisonerAlertsPage from '../../pages/appointments/create-and-edit/reviewPrisonerAlertsPage'
+import ReviewNonAssociationsEditPage from '../../pages/appointments/create-and-edit/reviewNonAssociationsEditPage'
 
 const tomorrow = addDays(new Date(), 1)
 const nextWeek = addDays(new Date(), 7)
@@ -41,6 +57,14 @@ context('Edit appointment', () => {
       '/users/jsmith',
       JSON.parse('{"name": "John Smith", "username": "jsmith", "authSource": "nomis"}'),
     )
+    cy.stubEndpoint('GET', '/prison/MDI/prisoners\\?term=potta&size=50', getPrisonPrisonersG0995GW)
+    cy.stubEndpoint('GET', '/prison/MDI/prisoners\\?term=saunders&size=50', getPrisonPrisonersG6123VU)
+    cy.stubEndpoint('GET', '/prisoner/G0995GW', getPrisonerG0995GW)
+    cy.stubEndpoint('GET', '/prisoner/G6123VU', getPrisonerG6123VU)
+    cy.stubEndpoint('POST', '/api/bookings/offenderNo/MDI/alerts', getOffenderAlertsG0995GW)
+    cy.stubEndpoint('POST', '/api/bookings/offenderNo/MDI/alerts', getOffenderAlertsG0995GWG6123VU)
+    cy.stubEndpoint('POST', '/non-associations/between', getNonAssociationsBetweenG0995GWA1350DZ)
+    cy.stubEndpoint('POST', '/prisoner-search/prisoner-numbers', getPrisonPrisonersG0995GWA1351DZ)
 
     cy.visit('/appointments/12')
   })
@@ -173,6 +197,112 @@ context('Edit appointment', () => {
         const extraInformationPage = Page.verifyOnPage(ExtraInformationPage)
         extraInformationPage.back()
         Page.verifyOnPage(AppointmentDetailsPage)
+      })
+    })
+  })
+  context('Add prisoners', () => {
+    it('Add a prisoner without a non-association', () => {
+      cy.stubEndpoint('POST', '/non-associations/between', [])
+      const appointmentDetailsPage = Page.verifyOnPage(AppointmentDetailsPage)
+      appointmentDetailsPage.addPrisonersLink().click()
+
+      const howToAddPrisonersPage = Page.verifyOnPage(HowToAddPrisonersPage)
+      howToAddPrisonersPage.selectOneByOne()
+      howToAddPrisonersPage.continue()
+
+      const selectPrisonerPage = Page.verifyOnPage(SelectPrisonerPage)
+      selectPrisonerPage.enterPrisonerNumber('potta')
+      selectPrisonerPage.searchButton().click()
+
+      selectPrisonerPage.continueButton().click()
+      const reviewPrisonersPage = Page.verifyOnPage(ReviewPrisonersPage)
+      reviewPrisonersPage.assertPrisonerInList('Potta, Aeticake')
+      reviewPrisonersPage.continue()
+
+      const reviewPrisonerAlertsPage = Page.verifyOnPage(ReviewPrisonerAlertsPage)
+      reviewPrisonerAlertsPage.continue()
+
+      Page.verifyOnPage(SchedulePage)
+    })
+    it('Add a single prisoner with a non-association with an existing prisoner', () => {
+      const appointmentDetailsPage = Page.verifyOnPage(AppointmentDetailsPage)
+      appointmentDetailsPage.addPrisonersLink().click()
+
+      const howToAddPrisonersPage = Page.verifyOnPage(HowToAddPrisonersPage)
+      howToAddPrisonersPage.selectOneByOne()
+      howToAddPrisonersPage.continue()
+
+      const selectPrisonerPage = Page.verifyOnPage(SelectPrisonerPage)
+      selectPrisonerPage.enterPrisonerNumber('potta')
+      selectPrisonerPage.searchButton().click()
+
+      selectPrisonerPage.continueButton().click()
+      const reviewPrisonersPage = Page.verifyOnPage(ReviewPrisonersPage)
+      reviewPrisonersPage.assertPrisonerInList('Potta, Aeticake')
+      reviewPrisonersPage.continue()
+
+      const reviewPrisonerAlertsPage = Page.verifyOnPage(ReviewPrisonerAlertsPage)
+      reviewPrisonerAlertsPage.continue()
+
+      const reviewNonAssociationsPage = Page.verifyOnPage(ReviewNonAssociationsEditPage)
+      reviewNonAssociationsPage.cards(1)
+      reviewNonAssociationsPage.getCard('G0995GW').then($data => {
+        expect($data.get(0).innerText).to.contain('David Winchurch')
+        expect($data.get(0).innerText).to.contain('Already attending')
+        expect($data.get(1).innerText).to.contain('A1350DZ')
+        expect($data.get(2).innerText).to.contain('2-2-024')
+        expect($data.get(3).innerText).to.contain('30 October 2024')
+      })
+
+      reviewNonAssociationsPage.removeAttendeeLink('G0995GW').click()
+      reviewNonAssociationsPage.header().should('contain.text', 'There are no attendees to add')
+      reviewNonAssociationsPage.addPrisonerButton().should('exist')
+    })
+    it('Add two prisoners who have non-associations with each other', () => {
+      cy.stubEndpoint('POST', '/non-associations/between', getNonAssociationsBetweenG0995GWG6123VU)
+      cy.stubEndpoint('POST', '/prisoner-search/prisoner-numbers', getPrisonPrisonersG0995GWG6123VU)
+
+      const appointmentDetailsPage = Page.verifyOnPage(AppointmentDetailsPage)
+      appointmentDetailsPage.addPrisonersLink().click()
+
+      const howToAddPrisonersPage = Page.verifyOnPage(HowToAddPrisonersPage)
+      howToAddPrisonersPage.selectOneByOne()
+      howToAddPrisonersPage.continue()
+
+      const selectPrisonerPage = Page.verifyOnPage(SelectPrisonerPage)
+      selectPrisonerPage.enterPrisonerNumber('potta')
+      selectPrisonerPage.searchButton().click()
+
+      selectPrisonerPage.continueButton().click()
+      const reviewPrisonersPage = Page.verifyOnPage(ReviewPrisonersPage)
+      reviewPrisonersPage.assertPrisonerInList('Potta, Aeticake')
+      reviewPrisonersPage.addAnotherPrisoner()
+
+      selectPrisonerPage.enterPrisonerNumber('saunders')
+      selectPrisonerPage.searchButton().click()
+      selectPrisonerPage.continueButton().click()
+
+      Page.verifyOnPage(ReviewPrisonersPage)
+      reviewPrisonersPage.continue()
+
+      const reviewPrisonerAlertsPage = Page.verifyOnPage(ReviewPrisonerAlertsPage)
+      reviewPrisonerAlertsPage.continue()
+
+      const reviewNonAssociationsPage = Page.verifyOnPage(ReviewNonAssociationsEditPage)
+      reviewNonAssociationsPage.cards(2)
+      reviewNonAssociationsPage.getCard('G0995GW').then($data => {
+        expect($data.get(0).innerText).to.contain('John Saunders')
+        expect($data.get(0).innerText).to.not.contain('Already attending')
+        expect($data.get(1).innerText).to.contain('G6123VU')
+        expect($data.get(2).innerText).to.contain('2-2-024')
+        expect($data.get(3).innerText).to.contain('30 October 2024')
+      })
+      reviewNonAssociationsPage.getCard('G6123VU').then($data => {
+        expect($data.get(0).innerText).to.contain('Aeticake Potta')
+        expect($data.get(0).innerText).to.not.contain('Already attending')
+        expect($data.get(1).innerText).to.contain('G0995GW')
+        expect($data.get(2).innerText).to.contain('1-3')
+        expect($data.get(3).innerText).to.contain('30 October 2024')
       })
     })
   })
