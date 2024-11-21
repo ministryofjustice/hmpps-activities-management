5484d45daf14a91ab3892915d5cfa0d8ce2b3391
Lance Munslow <lance@munslow.net>
2024-11-21 16:16:13 +0000

    SAA-2212: Fix checkboxes in absences view when moving from summary attendances view (#1248)
    Co-authored-by: Lance Munslow <lance.munslow@digital.justice.gov.uk>

--- server/routes/activities/daily-attendance-summary/handlers/applyFilters.ts
+++ server/routes/activities/daily-attendance-summary/handlers/applyFilters.ts
@@ -35,12 +35,19 @@ export class Filters {
 
 export default class ApplyFiltersRoutes {
   APPLY = async (req: Request, res: Response): Promise<void> => {
-    const { categoryFilters, reasonFilter, searchTerm, absenceReasonFilters, payFilters } = req.body
+    const { categoryFilters, reasonFilter, searchTerm, absenceReasonFilters, payFilters, isAbsencesFilter } = req.body
+
     req.session.attendanceSummaryJourney.categoryFilters = categoryFilters ?? []
-    req.session.attendanceSummaryJourney.absenceReasonFilters = absenceReasonFilters ?? []
-    req.session.attendanceSummaryJourney.payFilters = payFilters ?? []
     req.session.attendanceSummaryJourney.reasonFilter = reasonFilter ?? 'BOTH'
     req.session.attendanceSummaryJourney.searchTerm = searchTerm ?? null
+
+    if (isAbsencesFilter) {
+      req.session.attendanceSummaryJourney.absenceReasonFilters = absenceReasonFilters ?? []
+      req.session.attendanceSummaryJourney.payFilters = payFilters ?? []
+    } else {
+      req.session.attendanceSummaryJourney.absenceReasonFilters = undefined
+      req.session.attendanceSummaryJourney.payFilters = undefined
+    }
 
     res.redirect('back')
   }
