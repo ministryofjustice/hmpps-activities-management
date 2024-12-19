e8cc505f67d945f59306b5571f80f191454cf54c
Angela Pan <angela.pan@digital.justice.gov.uk>
2024-12-19 14:50:11 +0000

    fix missing static assets for dpr-frontend-lib (#1295)

--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -18,10 +18,21 @@ export default function setUpStaticResources(): Router {
   // TODO: Peer dependency of moj-frontend, consider helping to remove this from there and therefore as a dependency of this project
   router.use('/assets', express.static(path.join(process.cwd(), '/node_modules/jquery/dist'), cacheControl))
 
+  // moj assets
+  router.use(
+    '/moj/assets',
+    express.static(path.join(process.cwd(), '/node_modules/@ministryofjustice/frontend/moj/assets'), cacheControl),
+  )
+
   // Chart js
   router.use(
     '/assets/ext/chart.js',
     express.static(path.join(process.cwd(), '/node_modules/chart.js/dist/chart.umd.js')),
+  )
+
+  router.use(
+    '/assets/ext/chart.umd.js.map',
+    express.static(path.join(process.cwd(), '/node_modules/chart.js/dist/chart.umd.js.map')),
   )
 
   router.use(
