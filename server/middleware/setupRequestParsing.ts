0bfcb42bd23d6432ac1ea1b6e4bf47f88bd68351
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2024-12-31 12:17:42 +0000

    update url encoded request parameter limit to 2500

--- server/middleware/setupRequestParsing.ts
+++ server/middleware/setupRequestParsing.ts
@@ -3,6 +3,6 @@ import express, { Router } from 'express'
 export default function setUpWebRequestParsing(): Router {
   const router = express.Router()
   router.use(express.json())
-  router.use(express.urlencoded({ extended: true, parameterLimit: 10000 }))
+  router.use(express.urlencoded({ extended: true, parameterLimit: 2500 }))
   return router
 }
