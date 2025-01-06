d9345038673c68cae369729d99c89fabd4d7a938
Angela Pan <angela.pan@digital.justice.gov.uk>
2025-01-06 12:33:41 +0000

    removed product id variable as it's currently not required or relavent (#1310)

--- server/config.ts
+++ server/config.ts
@@ -47,7 +47,6 @@ interface RouteAuth {
 
 export default {
   buildNumber: get('BUILD_NUMBER', '1_0_0', requiredInProduction),
-  productId: get('PRODUCT_ID', 'UNASSIGNED', requiredInProduction),
   gitRef: get('GIT_REF', 'xxxxxxxxxxxxxxxxxxx', requiredInProduction),
   branchName: get('GIT_BRANCH', 'xxxxxxxxxxxxxxxxxxx', requiredInProduction),
   production,
