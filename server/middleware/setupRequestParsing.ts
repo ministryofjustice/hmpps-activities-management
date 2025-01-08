d3f25cb25b46eef69621f95196eec01c480e7473
Angela Pan <angela.pan@digital.justice.gov.uk>
2025-01-08 14:19:53 +0000

    SAA-2265 increase url encoded request parameter limit (#1307)
    * update url encoded request parameter limit to 2500

--- server/middleware/setupRequestParsing.ts
+++ server/middleware/setupRequestParsing.ts
@@ -3,6 +3,6 @@ import express, { Router } from 'express'
 export default function setUpWebRequestParsing(): Router {
   const router = express.Router()
   router.use(express.json())
-  router.use(express.urlencoded({ extended: true }))
+  router.use(express.urlencoded({ extended: true, parameterLimit: 2500 }))
   return router
 }
