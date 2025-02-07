a7558f020a17c6a65404b927891d40f395ab60d5
munslowl <lance.munslow@digital.justice.gov.uk>
2025-02-07 17:01:20 +0000

    junit

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   videosFolder: 'integration_tests/videos',
   reporter: 'junit',
   reporterOptions: {
-    mochaFile: "results/my-test-output.xml",
+    mochaFile: 'results/my-test-output.xml',
     toConsole: true,
   },
   taskTimeout: 60000,
