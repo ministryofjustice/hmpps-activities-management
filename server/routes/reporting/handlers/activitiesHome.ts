f5f879cbb207ae65bc26f7054e151042ef32d2c7
Natalie Rolph <natalie.clamp@digital.justice.gov.uk>
2025-02-28 11:10:12 +0000

    SAA-2385: wip

--- server/routes/reporting/handlers/activitiesHome.ts
+++ server/routes/reporting/handlers/activitiesHome.ts
@@ -8,7 +8,8 @@ export default class ActivitiesReportingHomeRoutes {
           title: 'Waitlist aggregation with averages',
           description: 'Some description of the report',
           href: '/dpr-reporting/waitlist-agg',
-          roleRequired: '',
+          // this is a test to check the roleRequired field works. We don't have the info as to which reports are restricted yet.
+          roleRequired: true,
         },
       ],
     }
--- server/routes/reporting/handlers/appointmentsHome.ts
+++ server/routes/reporting/handlers/appointmentsHome.ts
@@ -2,6 +2,16 @@ import { Request, Response } from 'express'
 
 export default class AppointmentsReportingHomeRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
-    res.render('pages/reporting/appointments-home')
+    const reports = [
+      {
+        title: 'Daily attendance summary',
+        description:
+          'Get attendance information for a single day, including who did not attend and where attendance has not been recorded yet.',
+        href: '/appointments/attendance-summary/select-date',
+        roleRequired: false,
+      },
+    ]
+
+    res.render('pages/reporting/appointments-home', { reports })
   }
 }
