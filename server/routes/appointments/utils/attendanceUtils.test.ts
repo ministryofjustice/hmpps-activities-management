3cf42a7ed972eb4e3be16eaeca33ef6ee115fea6
munslowl <lance.munslow@digital.justice.gov.uk>
2025-02-28 18:29:10 +0000

    SAA-2390: Add a unit test

--- server/routes/appointments/utils/attendanceUtils.test.ts
+++ server/routes/appointments/utils/attendanceUtils.test.ts
@@ -1,6 +1,7 @@
-import { AppointmentAttendanceSummary } from '../../../@types/activitiesAPI/types'
+import { AppointmentAttendanceSummary, AppointmentAttendeeByStatus } from '../../../@types/activitiesAPI/types'
 import EventTier from '../../../enum/eventTiers'
-import { getEventTierCounts } from './attendanceUtils'
+import { enhanceAppointment, getEventTierCounts } from './attendanceUtils'
+import { Prisoner } from '../../../@types/activities'
 
 describe('getEventTierCounts', () => {
   it('should calculate the tier summaries correctly', () => {
@@ -16,6 +17,53 @@ describe('getEventTierCounts', () => {
       tier1Count: 3,
       tier2Count: 10,
       foundationCount: 0,
+    })
+  })
+})
+
+describe('enhanceAppointment', () => {
+  let appointment: AppointmentAttendeeByStatus
+
+  const prisoner = {
+    prisonerName: 'AB123C',
+  } as Prisoner
+
+  beforeEach(() => {
+    appointment = {
+      appointmentId: 4567,
+      appointmentName: 'Canteen',
+      startDate: `2023-02-22`,
+      startTime: `11:23`,
+    } as AppointmentAttendeeByStatus
+  })
+
+  it ('should enhance appointment when appointment has an end time', () => {
+    appointment.endTime = '12:34'
+
+    const result = enhanceAppointment(appointment, prisoner)
+
+    expect(result).toEqual({
+      ...appointment,
+      ...prisoner,
+      time: `11:23 to 12:34`,
+      date: `22 February 2023`,
+      timeDateSortingValue: new Date(`2023-02-22T11:23`),
+      appointmentHref: '/appointments/attendance/4567/select-appointment',
+    })
+  })
+
+  it ('should enhance appointment when appointment has no end time', () => {
+    appointment.endTime = null
+
+    const result = enhanceAppointment(appointment, prisoner)
+
+    expect(result).toEqual({
+      ...appointment,
+      ...prisoner,
+      time: `11:23`,
+      date: `22 February 2023`,
+      timeDateSortingValue: new Date(`2023-02-22T11:23`),
+      appointmentHref: '/appointments/attendance/4567/select-appointment',
     })
   })
 })
