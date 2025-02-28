a1ada43f724006cd610333bc8266795964997afe
Natalie Rolph <natalie.clamp@digital.justice.gov.uk>
2025-02-28 11:35:13 +0000

    SAA-2385: Store reports for each list separately

--- server/routes/reporting/handlers/activitiesHome.ts
+++ server/routes/reporting/handlers/activitiesHome.ts
@@ -1,18 +1,8 @@
 import { Request, Response } from 'express'
+import reports from '../reportLists/activitiesReports'
 
 export default class ActivitiesReportingHomeRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
-    const reports = {
-      waitlists: [
-        {
-          title: 'Waitlist aggregation with averages',
-          description: 'Some description of the report',
-          href: '/dpr-reporting/waitlist-agg',
-          // this is a test to check the roleRequired field works. We don't have the info as to which reports are restricted yet.
-          roleRequired: true,
-        },
-      ],
-    }
     res.render('pages/reporting/activities-home', { reports })
   }
 }
--- server/routes/reporting/handlers/appointmentsHome.ts
+++ server/routes/reporting/handlers/appointmentsHome.ts
@@ -1,17 +1,8 @@
 import { Request, Response } from 'express'
+import reports from '../reportLists/appointmentsReports'
 
 export default class AppointmentsReportingHomeRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
-    const reports = [
-      {
-        title: 'Daily attendance summary',
-        description:
-          'Get attendance information for a single day, including who did not attend and where attendance has not been recorded yet.',
-        href: '/appointments/attendance-summary/select-date',
-        roleRequired: false,
-      },
-    ]
-
     res.render('pages/reporting/appointments-home', { reports })
   }
 }
