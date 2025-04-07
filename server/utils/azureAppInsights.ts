8d09629e66f2346fb5d9f8c90f94ffac07e8d770
Natalie Rolph <natalie.clamp@digital.justice.gov.uk>
2025-04-07 16:12:31 +0100

    SAA-2453: App insights utils update

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,18 +1,19 @@
-import { Contracts, setup, defaultClient, TelemetryClient, DistributedTracingModes } from 'applicationinsights'
-import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
+import {
+  defaultClient,
+  DistributedTracingModes,
+  getCorrelationContext,
+  setup,
+  type TelemetryClient,
+} from 'applicationinsights'
+import { RequestHandler } from 'express'
 import type { ApplicationInfo } from '../applicationInfo'
-
-export type ContextObject = {
-  /* eslint-disable  @typescript-eslint/no-explicit-any */
-  [name: string]: any
-}
 
 export function initialiseAppInsights(): void {
   if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
     // eslint-disable-next-line no-console
     console.log('Enabling azure application insights')
 
-    setup()?.setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
+    setup().setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
   }
 }
 
@@ -20,33 +21,31 @@ export function buildAppInsightsClient(
   { applicationName, buildNumber }: ApplicationInfo,
   overrideName?: string,
 ): TelemetryClient {
-  if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING && defaultClient) {
+  if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
     defaultClient.context.tags['ai.cloud.role'] = overrideName || applicationName
     defaultClient.context.tags['ai.application.ver'] = buildNumber
-    defaultClient.addTelemetryProcessor(addUserDataToRequests)
+
+    defaultClient.addTelemetryProcessor(({ tags, data }, contextObjects) => {
+      const operationNameOverride = contextObjects.correlationContext?.customProperties?.getProperty('operationName')
+      if (operationNameOverride) {
+        tags['ai.operation.name'] = data.baseData.name = operationNameOverride // eslint-disable-line no-param-reassign,no-multi-assign
+      }
+      return true
+    })
+
     return defaultClient
   }
   return null
 }
 
-/**
- * Adds extra data in the requests logged for the service, to identify unique username and active caseload.
- * @param envelope
- * @param contextObjects
- */
-export function addUserDataToRequests(envelope: EnvelopeTelemetry, contextObjects: ContextObject) {
-  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
-  if (isRequest) {
-    const { username, activeCaseLoadId } = contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
-    if (username) {
-      const { properties } = envelope.data.baseData
-      // eslint-disable-next-line no-param-reassign
-      envelope.data.baseData.properties = {
-        username,
-        activeCaseLoadId,
-        ...properties,
+export function appInsightsMiddleware(): RequestHandler {
+  return (req, res, next) => {
+    res.prependOnceListener('finish', () => {
+      const context = getCorrelationContext()
+      if (context && req.route) {
+        context.customProperties.setProperty('operationName', `${req.method} ${req.route?.path}`)
       }
-    }
+    })
+    next()
   }
-  return true
 }
