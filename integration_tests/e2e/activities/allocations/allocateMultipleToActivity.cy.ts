cf0cf83e51d960b3db5dfd2a0221438e3f92fdcf
Nat Rolph <84783598+natclamp-moj@users.noreply.github.com>
2025-04-08 15:57:11 +0100

    SAA-2442: Add int tests (#1498)

--- integration_tests/e2e/activities/allocations/allocateMultipleToActivity.cy.ts
+++ integration_tests/e2e/activities/allocations/allocateMultipleToActivity.cy.ts
@@ -38,7 +38,6 @@ context('Allocate multiple one by one to an activity', () => {
     cy.task('stubSignIn')
     cy.stubEndpoint('GET', '/prison/MDI/activities\\?excludeArchived=true', getActivities)
     cy.stubEndpoint('GET', '/activities/(\\d)*/schedules', getSchedulesInActivity)
-    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A5015DY', getCandidateSuitability)
     cy.stubEndpoint('GET', '/incentive/prison-levels/MDI', moorlandIncentiveLevels)
     cy.stubEndpoint('GET', '/schedules/2/allocations\\?activeOnly=true&includePrisonerSummary=true', getAllocations)
     cy.stubEndpoint('POST', '/prisons/MDI/prisoner-allocations', prisonerAllocations)
@@ -56,9 +55,6 @@ context('Allocate multiple one by one to an activity', () => {
     cy.stubEndpoint('GET', '/prisoner/A8644DY', getPrisonerA8644DY)
     cy.stubEndpoint('GET', '/prisoner/A1351DZ', getPrisonerA1351DZ)
     cy.stubEndpoint('GET', '/schedules/2/allocations\\?includePrisonerSummary=true', [])
-    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1350DZ', getCandidateSuitability)
-    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A8644DY', getCandidateSuitability)
-    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1351DZ', getCandidateSuitability)
 
     resetActivityAndScheduleStubs({ activityStartDate: subWeeks(new Date(), 2), reducedPayOptions: true })
 
@@ -66,6 +62,10 @@ context('Allocate multiple one by one to an activity', () => {
   })
 
   it('should be able to allocate when selecting multiple inmates', () => {
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1350DZ', getCandidateSuitability)
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A8644DY', getCandidateSuitability)
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1351DZ', getCandidateSuitability)
+
     const indexPage = Page.verifyOnPage(IndexPage)
     indexPage.activitiesCard().click()
 
@@ -167,5 +167,158 @@ context('Allocate multiple one by one to an activity', () => {
     cy.location().should(loc => {
       expect(loc.pathname).to.eq('/activities/allocation-dashboard')
     })
+  })
+  it('follows correct routing when requirements page is skipped and end date is to be chosen', () => {
+    const candidateSuitabilityCopy = { ...getCandidateSuitability }
+    candidateSuitabilityCopy.workplaceRiskAssessment.suitable = true
+    candidateSuitabilityCopy.education.suitable = true
+    candidateSuitabilityCopy.releaseDate.suitable = true
+
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1350DZ', candidateSuitabilityCopy)
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A8644DY', candidateSuitabilityCopy)
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1351DZ', candidateSuitabilityCopy)
+
+    const indexPage = Page.verifyOnPage(IndexPage)
+    indexPage.activitiesCard().click()
+
+    const activitiesIndexPage = Page.verifyOnPage(ActivitiesIndexPage)
+    activitiesIndexPage.allocateToActivitiesCard().click()
+
+    const manageActivitiesPage = Page.verifyOnPage(ManageActivitiesDashboardPage)
+    manageActivitiesPage.allocateToActivityCard().should('contain.text', 'Manage allocations')
+    manageActivitiesPage.allocateToActivityCard().click()
+
+    const activitiesPage = Page.verifyOnPage(ActivitiesDashboardPage)
+    activitiesPage.activityRows().should('have.length', 3)
+    activitiesPage.selectActivityWithName('English level 1')
+
+    const allocatePage = Page.verifyOnPage(AllocationDashboard)
+    allocatePage.allocatedPeopleRows().should('have.length', 3)
+    allocatePage.nonAssociationsLink('G4793VF').contains('View non-associations')
+    allocatePage.nonAssociationsLink('A1351DZ').should('not.exist')
+    allocatePage.nonAssociationsLink('B1351RE').contains('View non-associations')
+    allocatePage.tabWithTitle('Entry level English 1 schedule').click()
+    allocatePage.activeTimeSlots().should('have.length', 1)
+
+    allocatePage.tabWithTitle('Other people').click()
+    allocatePage.allocateGroupLink()
+
+    const setUpPrisonerListMethodPage = Page.verifyOnPage(SetUpPrisonerListMethodPage)
+    setUpPrisonerListMethodPage.selectHowToAddDecisionRadio(HowToAddOptions.SEARCH)
+    setUpPrisonerListMethodPage.getButton('Continue').click()
+
+    const selectPrisonerPage = Page.verifyOnPage(SelectPrisonerPage)
+    selectPrisonerPage.caption().should('contain.text', 'Entry level English 1')
+    selectPrisonerPage.enterQuery('s')
+    selectPrisonerPage.getButton('Search').click()
+    selectPrisonerPage.selectRadio('A1350DZ').click()
+    selectPrisonerPage.selectPrisonerAndContinue()
+
+    selectPrisonerPage.addAnotherPersonLink()
+    selectPrisonerPage.enterQuery('s')
+    selectPrisonerPage.getButton('Search').click()
+    selectPrisonerPage.selectRadio('A8644DY').click()
+    selectPrisonerPage.selectPrisonerAndContinue()
+
+    selectPrisonerPage.addAnotherPersonLink()
+    selectPrisonerPage.enterQuery('s')
+    selectPrisonerPage.getButton('Search').click()
+    selectPrisonerPage.selectRadio('A1351DZ').click()
+    selectPrisonerPage.selectPrisonerAndContinue()
+
+    selectPrisonerPage.inmateRows().should('have.length', 3)
+    selectPrisonerPage.continue()
+
+    // skips the activity requirements page as all requirements are met
+
+    const startDatePage = Page.verifyOnPage(StartDatePage)
+    startDatePage.selectNextSession()
+    startDatePage.continue()
+
+    const endDateOptionPage = Page.verifyOnPage(EndDateOptionPage)
+    endDateOptionPage.addEndDate('Yes')
+    endDateOptionPage.continue()
+
+    const endDatePage = Page.verifyOnPage(EndDatePage)
+    const endDate = addMonths(new Date(), 8)
+    endDatePage.selectDatePickerDate(endDate)
+    endDatePage.continue()
+
+    // Gets back onto the multiple persion version of the pay-band page
+    Page.verifyOnPage(PayBandMultiplePage)
+  })
+  it('Follows correct routing when requirements page is skipped and end date is not set', () => {
+    const candidateSuitabilityCopy = { ...getCandidateSuitability }
+    candidateSuitabilityCopy.workplaceRiskAssessment.suitable = true
+    candidateSuitabilityCopy.education.suitable = true
+    candidateSuitabilityCopy.releaseDate.suitable = true
+
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1350DZ', candidateSuitabilityCopy)
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A8644DY', candidateSuitabilityCopy)
+    cy.stubEndpoint('GET', '/schedules/2/suitability\\?prisonerNumber=A1351DZ', candidateSuitabilityCopy)
+
+    const indexPage = Page.verifyOnPage(IndexPage)
+    indexPage.activitiesCard().click()
+
+    const activitiesIndexPage = Page.verifyOnPage(ActivitiesIndexPage)
+    activitiesIndexPage.allocateToActivitiesCard().click()
+
+    const manageActivitiesPage = Page.verifyOnPage(ManageActivitiesDashboardPage)
+    manageActivitiesPage.allocateToActivityCard().should('contain.text', 'Manage allocations')
+    manageActivitiesPage.allocateToActivityCard().click()
+
+    const activitiesPage = Page.verifyOnPage(ActivitiesDashboardPage)
+    activitiesPage.activityRows().should('have.length', 3)
+    activitiesPage.selectActivityWithName('English level 1')
+
+    const allocatePage = Page.verifyOnPage(AllocationDashboard)
+    allocatePage.allocatedPeopleRows().should('have.length', 3)
+    allocatePage.nonAssociationsLink('G4793VF').contains('View non-associations')
+    allocatePage.nonAssociationsLink('A1351DZ').should('not.exist')
+    allocatePage.nonAssociationsLink('B1351RE').contains('View non-associations')
+    allocatePage.tabWithTitle('Entry level English 1 schedule').click()
+    allocatePage.activeTimeSlots().should('have.length', 1)
+
+    allocatePage.tabWithTitle('Other people').click()
+    allocatePage.allocateGroupLink()
+
+    const setUpPrisonerListMethodPage = Page.verifyOnPage(SetUpPrisonerListMethodPage)
+    setUpPrisonerListMethodPage.selectHowToAddDecisionRadio(HowToAddOptions.SEARCH)
+    setUpPrisonerListMethodPage.getButton('Continue').click()
+
+    const selectPrisonerPage = Page.verifyOnPage(SelectPrisonerPage)
+    selectPrisonerPage.caption().should('contain.text', 'Entry level English 1')
+    selectPrisonerPage.enterQuery('s')
+    selectPrisonerPage.getButton('Search').click()
+    selectPrisonerPage.selectRadio('A1350DZ').click()
+    selectPrisonerPage.selectPrisonerAndContinue()
+
+    selectPrisonerPage.addAnotherPersonLink()
+    selectPrisonerPage.enterQuery('s')
+    selectPrisonerPage.getButton('Search').click()
+    selectPrisonerPage.selectRadio('A8644DY').click()
+    selectPrisonerPage.selectPrisonerAndContinue()
+
+    selectPrisonerPage.addAnotherPersonLink()
+    selectPrisonerPage.enterQuery('s')
+    selectPrisonerPage.getButton('Search').click()
+    selectPrisonerPage.selectRadio('A1351DZ').click()
+    selectPrisonerPage.selectPrisonerAndContinue()
+
+    selectPrisonerPage.inmateRows().should('have.length', 3)
+    selectPrisonerPage.continue()
+
+    // skips the activity requirements page as all requirements are met
+
+    const startDatePage = Page.verifyOnPage(StartDatePage)
+    startDatePage.selectNextSession()
+    startDatePage.continue()
+
+    const endDateOptionPage = Page.verifyOnPage(EndDateOptionPage)
+    endDateOptionPage.addEndDate('No')
+    endDateOptionPage.continue()
+
+    // Gets back onto the multiple persion version of the pay-band page
+    Page.verifyOnPage(PayBandMultiplePage)
   })
 })
