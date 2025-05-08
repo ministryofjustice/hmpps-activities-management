c7fbbb55db37ac13e3d62bd98d5938142de836a1
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-05-08 16:29:35 +0100

    Added extra service tests

--- server/services/bankHolidayService.test.ts
+++ server/services/bankHolidayService.test.ts
@@ -5,6 +5,78 @@ import BankHolidayService from './bankHolidayService'
 
 jest.mock('../data/bankHolidaysClient')
 
+const mockBankHolidaysList = {
+  'england-and-wales': {
+    division: 'england-and-wales',
+    events: [
+      {
+        title: "New Year's Day",
+        date: '2019-01-01',
+        notes: '',
+        bunting: true,
+      },
+      {
+        title: 'Good Friday',
+        date: '2019-04-19',
+        notes: '',
+        bunting: false,
+      },
+      {
+        title: 'Easter Monday',
+        date: '2019-04-22',
+        notes: '',
+        bunting: true,
+      },
+    ],
+  },
+  scotland: {
+    division: 'scotland',
+    events: [
+      {
+        title: 'Early May bank holiday',
+        date: '2019-05-06',
+        notes: '',
+        bunting: true,
+      },
+      {
+        title: 'Spring bank holiday',
+        date: '2019-05-27',
+        notes: '',
+        bunting: true,
+      },
+      {
+        title: 'Summer bank holiday',
+        date: '2019-08-26',
+        notes: '',
+        bunting: true,
+      },
+    ],
+  },
+  'northern-ireland': {
+    division: 'northern-ireland',
+    events: [
+      {
+        title: 'Christmas Day',
+        date: '2019-12-25',
+        notes: '',
+        bunting: true,
+      },
+      {
+        title: 'Boxing Day',
+        date: '2019-12-26',
+        notes: '',
+        bunting: true,
+      },
+      {
+        title: 'New Year’s Day',
+        date: '2020-01-01',
+        notes: '',
+        bunting: true,
+      },
+    ],
+  },
+}
+
 describe('Bank Holiday service', () => {
   let bankHolidaysClient: jest.Mocked<BankHolidaysClient>
   let bankHolidaysService: BankHolidayService
@@ -21,38 +93,34 @@ describe('Bank Holiday service', () => {
   })
 
   describe('getUkBankHolidays', () => {
-    it('should get a list of bank holidays', async () => {
+    it('should get a list of bank holidays for england-and-wales', async () => {
       const expectedResult = [new Date('2019-01-01'), new Date('2019-04-19'), new Date('2019-04-22')]
 
-      when(bankHolidaysClient.getBankHolidays).mockResolvedValue({
-        'england-and-wales': {
-          division: 'england-and-wales',
-          events: [
-            {
-              title: "New Year's Day",
-              date: '2019-01-01',
-              notes: '',
-              bunting: true,
-            },
-            {
-              title: 'Good Friday',
-              date: '2019-04-19',
-              notes: '',
-              bunting: false,
-            },
-            {
-              title: 'Easter Monday',
-              date: '2019-04-22',
-              notes: '',
-              bunting: true,
-            },
-          ],
-        },
-        scotland: undefined,
-        'northern-ireland': undefined,
-      })
+      when(bankHolidaysClient.getBankHolidays).mockResolvedValue(mockBankHolidaysList)
 
       const actualResult = await bankHolidaysService.getUkBankHolidays('england-and-wales', user)
+
+      expect(actualResult).toEqual(expectedResult)
+      expect(bankHolidaysClient.getBankHolidays).toHaveBeenCalledWith(user)
+    })
+
+    it('should get a list of bank holidays for scotland', async () => {
+      const expectedResult = [new Date('2019-05-06'), new Date('2019-05-27'), new Date('2019-08-26')]
+
+      when(bankHolidaysClient.getBankHolidays).mockResolvedValue(mockBankHolidaysList)
+
+      const actualResult = await bankHolidaysService.getUkBankHolidays('scotland', user)
+
+      expect(actualResult).toEqual(expectedResult)
+      expect(bankHolidaysClient.getBankHolidays).toHaveBeenCalledWith(user)
+    })
+
+    it('should get a list of bank holidays for northern-ireland', async () => {
+      const expectedResult = [new Date('2019-12-25'), new Date('2019-12-26'), new Date('2020-01-01')]
+
+      when(bankHolidaysClient.getBankHolidays).mockResolvedValue(mockBankHolidaysList)
+
+      const actualResult = await bankHolidaysService.getUkBankHolidays('northern-ireland', user)
 
       expect(actualResult).toEqual(expectedResult)
       expect(bankHolidaysClient.getBankHolidays).toHaveBeenCalledWith(user)
