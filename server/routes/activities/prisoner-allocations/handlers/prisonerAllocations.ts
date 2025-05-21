3b5cd1c2e7716a36784c1a7e5e374193dee9923c
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-05-21 16:45:30 +0100

    Added integration tests journey setup

--- server/routes/activities/prisoner-allocations/handlers/prisonerAllocations.ts
+++ server/routes/activities/prisoner-allocations/handlers/prisonerAllocations.ts
@@ -5,17 +5,10 @@ export default class PrisonerAllocationsHandler {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
   GET = async (req: Request, res: Response) => {
-    // const { prisonerNumber } = req.params
-
-    // const allocations = await this.activitiesService.getAllocations(prisonerNumber)
     res.render('pages/activities/prisoner-allocations/dashboard')
   }
 
   POST = async (req: Request, res: Response) => {
-    const { prisonId } = req.params
-    // const { allocationId } = req.body
-
-    // await this.activitiesService.updateAllocation(prisonId, allocationId)
-    res.redirect(`/activities/prisoner-allocations/${prisonId}`)
+    res.redirect(`/activities/prisoner-allocations/`)
   }
 }
