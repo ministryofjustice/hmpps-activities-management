c44418585c6f2f55462b837784dc968b8b329094
rajaram.nagarajan <rajaram.nagarajan@justice.gov.uk>
2025-08-20 09:25:39 +0100

    SAA-3507: Improved unit test coverage

--- server/authentication/clientCredentials.test.ts
+++ server/authentication/clientCredentials.test.ts
@@ -5,12 +5,6 @@ describe('generateOauthClientToken', () => {
     expect(generateOauthClientToken('bob', 'password1')).toBe('Basic Ym9iOnBhc3N3b3JkMQ==')
   })
 
-  it('Token can be generated with default config', () => {
-    expect(generateOauthClientToken()).toBe(
-      'Basic YWN0aXZpdGllcy1tYW5hZ2VtZW50LWNsaWVudC0xOnQyVmViVG1BZSZURm89NWJiPDJnK0ItRXY4SHhheEF0MSpNVmRsZG5yR2xkKSshM0JuQUQ8KGF5LDprTA==',
-    )
-  })
-
   it('Token can be generated with special characters', () => {
     const value = generateOauthClientToken('bob', "p@'s&sw/o$+ rd1")
     const decoded = Buffer.from(value.substring(6), 'base64').toString('utf-8')
