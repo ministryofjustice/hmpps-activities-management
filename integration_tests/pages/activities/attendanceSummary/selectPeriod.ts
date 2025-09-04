9de99a8e35c145d593b2afe1e82a635b5f5bf1c2
Vamsi-QA <vamsi.mandapati@digital.justice.gov.uk>
2025-09-04 12:17:55 +0100

    set prisoners to not required tests

--- integration_tests/pages/activities/attendanceSummary/selectPeriod.ts
+++ integration_tests/pages/activities/attendanceSummary/selectPeriod.ts
@@ -4,4 +4,26 @@ export default class SelectPeriodPage extends Page {
   constructor() {
     super('select-period-page')
   }
+
+  selectADifferentDate = () => {
+    this.getInputByLabel('A different date').click()
+    cy.log('Clicked "A different date" to show the date input.')
+
+    // Open the datepicker
+    cy.get('.moj-datepicker__toggle').click()
+    cy.log('Opened the datepicker.')
+  }
+
+  pickDateFromToday = (days: number) => {
+    const target = new Date()
+    target.setDate(target.getDate() + days)
+    const targetDay = target.getDate().toString()
+
+    cy.get('.moj-datepicker__calendar').should('be.visible')
+    cy.get('.moj-datepicker__calendar button')
+      .contains(new RegExp(`^${targetDay}$`))
+      .click()
+
+    cy.get('.moj-datepicker__dialog').should('not.be.visible')
+  }
 }
