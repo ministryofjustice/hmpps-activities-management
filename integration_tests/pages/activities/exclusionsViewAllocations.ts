a4ae02cd5abfef973fdc7a4455420e1dbd55c294
David Thomas <david.thomas1@digital.justice.gov.uk>
2025-09-12 16:24:17 +0100

    SAA-3575: Add fixtures / update tests

--- integration_tests/pages/activities/exclusionsViewAllocations.ts
+++ integration_tests/pages/activities/exclusionsViewAllocations.ts
@@ -1,11 +1,13 @@
 import Page from '../page'
 
-export default class exclusionsViewAllocations extends Page {
+export default class ExclusionsViewAllocations extends Page {
   constructor() {
     super('view-allocations-page')
   }
 
   viewSuspendedLinks = (): Cypress.Chainable => cy.get('[data-qa=suspension-link]')
+
+  suspensionBadge = (): Cypress.Chainable => cy.get('[data-qa=suspension-badge]')
 
   firstSuspensionLink = (): Cypress.Chainable => cy.contains('View suspension').click()
 }
