3903ce1a63f15056f3759bc7813fbbf166bb6ad2
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-09-23 14:22:05 +0100

    Added cypress page

---
+++ integration_tests/pages/recordAttendance/attend-all/chooseDetailsByActivityLocationPage.ts
@@ -0,0 +1,31 @@
+import Page from '../../page'
+
+export default class ChooseDetailsByActivityLocationPage extends Page {
+  constructor() {
+    super('choose-details-by-activity-attendance-page')
+  }
+
+  radioTodayClick = (): Cypress.Chainable => cy.get('#datePresetOption').click()
+
+  radioTomorrowClick = (): Cypress.Chainable => cy.get('#datePresetOption-2').click()
+
+  radioOtherClick = (): Cypress.Chainable => cy.get('datePresetOption-3').click()
+
+  checkboxAMClick = (): Cypress.Chainable => cy.get('#timePeriod').click()
+
+  checkboxPMClick = (): Cypress.Chainable => cy.get('#timePeriod-2').click()
+
+  checkboxEDClick = (): Cypress.Chainable => cy.get('#timePeriod-3').click()
+
+  radioSearchForLocationClick = (): Cypress.Chainable => cy.get('#locationType').click()
+
+  searchBoxLocation = (): Cypress.Chainable => cy.get('#location')
+
+  radioInCellClick = (): Cypress.Chainable => cy.get('#locationType-2').click()
+
+  radioOffWingClick = (): Cypress.Chainable => cy.get('#locationType-3').click()
+
+  radioOnWingClick = (): Cypress.Chainable => cy.get('#locationType-4').click()
+
+  selectLocation = (location: string) => cy.get(`[value="${location}"]`).click()
+}
