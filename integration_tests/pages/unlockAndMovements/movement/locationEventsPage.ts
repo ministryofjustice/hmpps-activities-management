4209364ec911ad72f59aff6266a6f6ab7f6efc9b
Nat Rolph <84783598+natclamp-moj@users.noreply.github.com>
2025-11-24 12:51:58 +0000

    SAA-3834: Movement list tag for not required (#1893)

--- integration_tests/pages/unlockAndMovements/movement/locationEventsPage.ts
+++ integration_tests/pages/unlockAndMovements/movement/locationEventsPage.ts
@@ -4,4 +4,6 @@ export default class LocationEventsPage extends AbstractEventsPage {
   constructor() {
     super('movement-list-location-events-page')
   }
+
+  table = (): Cypress.Chainable => cy.get('[data-qa="location-1-prisoner-events"]')
 }
