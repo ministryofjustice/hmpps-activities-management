2ce4dfdc15e9e1d0be1335c7446f49069214435a
Natalie Rolph <natalie.rolph@justice.gov.uk>
2025-12-08 16:38:11 +0000

    SAA-3872: Implement the allowlist package

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,11 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+  allowlist: {
+    'node_modules/cypress@15.7.0': 'ALLOW',
+    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/esbuild@0.27.0': 'ALLOW',
+    'node_modules/tsx/node_modules/esbuild@0.25.12': 'ALLOW',
+    'node_modules/unrs-resolver@1.9.1': 'ALLOW',
+  },
+})
