4f7896e874eb4f10ae0c04c7eb0ff3da985cfd94
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-12-08 13:33:07 +0000

    BVLS-1177 show extra info tag on the movement list when there are prisoner comments.

--- integration_tests/pages/unlockAndMovements/movement/locationEventsPage.ts
+++ integration_tests/pages/unlockAndMovements/movement/locationEventsPage.ts
@@ -6,4 +6,10 @@ export default class LocationEventsPage extends AbstractEventsPage {
   }
 
   table = (): Cypress.Chainable => cy.get('[data-qa="location-1-prisoner-events"]')
+
+  appointmentLinkIsPresent = (identifier: string) => cy.get(`a[href="/appointments/${identifier}"]`).should('exist')
+
+  extraInfoTagIsPresent = (identifier: string) => cy.get(`[data-qa="extra-info-tag-${identifier}"]`).should('exist')
+
+  extraInfoTagIsAbsent = (identifier: string) => cy.get(`[data-qa="extra-info-tag-${identifier}"]`).should('not.exist')
 }
