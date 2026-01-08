5474a1f17b5e6a9484693fd8a6562bb6f0db8127
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-01-08 17:31:30 +0000

    BAVL-1114 tweak to content for editing of prisoner notes to let user now changes may not make it onto movement slips in time. (#1933)

--- server/views/pages/appointments/create-and-edit/extra-information.njk.test.ts
+++ server/views/pages/appointments/create-and-edit/extra-information.njk.test.ts
@@ -313,7 +313,7 @@ describe('Views - Appointments Management - Extra Information, with toggle on', 
     expect($('h1').text().trim()).toContain(`Change the extra information`)
 
     expect($('[data-qa=first-paragraph]').text().trim()).toContain(
-      'Add or edit information the prisoner needs to know about their appointment, like something they need to bring, or do beforehand. This will appear on movement slips and will be seen by the prisoner.',
+      'Add or edit information the prisoner needs to know about their appointment, like something they need to bring, or do beforehand. This will appear on movement slips and will be seen by the prisoner. Changes will not appear on movement slips that have already been printed.',
     )
     expect($('[data-qa=second-paragraph]').text().trim()).toEqual(
       'Do not add anything that should not be seen by or shared with a prisoner.',
