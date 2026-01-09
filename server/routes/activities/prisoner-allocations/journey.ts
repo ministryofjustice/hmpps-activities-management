7b11256589ae9fc94578e8261336be07bfa9f067
David Thomas <david.thomas1@digital.justice.gov.uk>
2026-01-09 15:27:59 +0000

    SAA-3835: Waitlist pending dropdown bug (#1937)
    * SAA-3835: Waitlist pending dropdown bug
    
    * SAA-3835: peer review refactor

--- server/routes/activities/prisoner-allocations/journey.ts
+++ server/routes/activities/prisoner-allocations/journey.ts
@@ -1,9 +1,20 @@
+import { ActivitySummary } from '../../../@types/activitiesAPI/types'
+
 export type PrisonerAllocationsJourney = {
   activityName?: string
-  status?: 'PENDING' | 'APPROVED' | 'DECLINED' | 'ALLOCATED' | 'REMOVED'
+  status?: 'PENDING' | 'APPROVED' | 'DECLINED' | 'ALLOCATED' | 'REMOVED' | 'WITHDRAWN'
   scheduleId?: number
   applicationId?: number
   applicationDate?: string
   requestedBy?: string
   comments?: string
+}
+
+export type WaitlistApplication = {
+  activityId: number
+  status: 'PENDING' | 'APPROVED' | 'DECLINED' | 'ALLOCATED' | 'REMOVED' | 'WITHDRAWN'
+}
+
+export type EnhancedWaitlistApplication = WaitlistApplication & {
+  activity?: ActivitySummary
 }
