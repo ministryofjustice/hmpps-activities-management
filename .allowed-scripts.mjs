2c04850f20044bf729d37b2f8e03f5f8fe890c49
Natalie Rolph <natalie.rolph@justice.gov.uk>
2026-01-12 13:28:33 +0000

    Allow updated cypress version

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,7 +2,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/cypress@15.7.0': 'ALLOW',
+    'node_modules/cypress@15.8.2': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/esbuild@0.27.0': 'ALLOW',
     'node_modules/tsx/node_modules/esbuild@0.25.12': 'ALLOW',
