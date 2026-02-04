6dd1cd101afa93d5332c08d663616e040e46b2ef
Ashley Gyngell <ashley.gyngell@justice.gov.uk>
2026-02-04 12:44:18 +0000

    SAA-3661: Add withdrawn status types to waitlist

--- server/enum/waitingListStatus.ts
+++ server/enum/waitingListStatus.ts
@@ -2,6 +2,7 @@ export enum WaitingListStatusOptions {
   APPROVED = 'APPROVED',
   DECLINED = 'DECLINED',
   PENDING = 'PENDING',
+  WITHDRAWN = 'WITHDRAWN',
 }
 
 export enum WaitingListStatus {
@@ -9,4 +10,20 @@ export enum WaitingListStatus {
   APPROVED = 'APPROVED',
   DECLINED = 'DECLINED',
   ALLOCATED = 'ALLOCATED',
+}
+
+export enum WaitingListStatusWithWithdrawn {
+  ALLOCATED = 'ALLOCATED',
+  APPROVED = 'APPROVED',
+  PENDING = 'PENDING',
+  DECLINED = 'DECLINED',
+  WITHDRAWN = 'WITHDRAWN',
+}
+
+export enum WaitingListStatusDescriptions {
+  APPROVED = `Add the applicant to the waitlist. They're ready to be allocated.`,
+  DECLINED = `Reject the application and inform the person concerned.`,
+  PENDING = "Add the applicant to the waitlist as 'Pending'. " +
+    'Follow your usual procedure to check if they can be allocated.',
+  WITHDRAWN = 'will be removed from the waitlist.',
 }
