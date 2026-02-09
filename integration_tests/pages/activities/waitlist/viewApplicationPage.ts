07e1eabf121de0da858af79d3f90d1b1a7179c4d
Ashley Gyngell <ashley.gyngell@justice.gov.uk>
2026-02-09 10:42:49 +0000

    SAA-4033: Add tests for waitlist dashboard/edit pages

---
+++ integration_tests/pages/activities/waitlist/viewApplicationPage.ts
@@ -0,0 +1,31 @@
+import Page from '../../page'
+
+export default class ViewApplicationPage extends Page {
+  constructor() {
+    super('view-application-page')
+  }
+
+  successBanner = (): Cypress.Chainable => cy.get('.govuk-notification-banner--success')
+
+  checkApplicationStatus = (status: string) => {
+    cy.get('.govuk-summary-list__value').eq(0).should('contain.text', status)
+  }
+
+  checkActivityRequested = (activity: string) => {
+    cy.get('.govuk-summary-list__value').eq(1).should('contain.text', activity)
+  }
+
+  checkRequester = (requester: string) => {
+    cy.get('.govuk-summary-list__value').eq(2).should('contain.text', requester)
+  }
+
+  checkDateOfRequest = (date: string) => {
+    cy.get('.govuk-summary-list__value').eq(3).should('contain.text', date)
+  }
+
+  checkComments = (comments: string) => {
+    cy.get('.govuk-summary-list__value').eq(4).should('contain.text', comments)
+  }
+
+  changeStatusLink = (): Cypress.Chainable => cy.get('a').contains('Change').first()
+}
