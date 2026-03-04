c5ae76f0ebe473dbcc36fbe1e293e7f6ba8e6fda
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2026-03-04 09:37:33 +0000

    Updated allowlist for node_modules/cypress@15.11.0

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,7 +2,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/cypress@15.10.0': 'ALLOW',
+    'node_modules/cypress@15.11.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/esbuild@0.27.3': 'ALLOW',
     'node_modules/unrs-resolver@1.9.1': 'ALLOW',
