0a39bc2fae83a9fba06e23572a9710a2afa43636
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2026-03-04 09:07:43 +0000

    Updated allowlist for node_modules/esbuild@0.27.3

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,7 +4,7 @@ export default configureAllowedScripts({
   allowlist: {
     'node_modules/cypress@15.10.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/esbuild@0.27.2': 'ALLOW',
+    'node_modules/esbuild@0.27.3': 'ALLOW',
     'node_modules/unrs-resolver@1.9.1': 'ALLOW',
   },
 })
