ab3f79e622910be54318331ac020b8fec0e09ab6
Natalie Rolph <natalie.rolph@justice.gov.uk>
2026-03-19 14:29:39 +0000

    Update minor deps

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,9 +2,10 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/cypress@15.11.0': 'ALLOW',
+    'node_modules/cypress@15.12.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/esbuild@0.27.3': 'ALLOW',
+    'node_modules/esbuild@0.27.4': 'ALLOW',
     'node_modules/unrs-resolver@1.9.1': 'ALLOW',
+    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
   },
 })
