1db57c35b35a288ecf5142519a7a4ed48bf1cba6
spencer.howe <spencer.howe@justice.gov.uk>
2026-08-11 12:43:01 +0100

    feat: tighten cypress setup, improve linter, conditional install for missing cache

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 export default [
   ...hmppsConfig({
     extraIgnorePaths: ['assets', 'frontend', 'cypress.json', 'reporter-config.json', 'server/@types'],
+    // HMPPS eslint config doesn't recognise e2e as a test folder, so we need to add it here to avoid linting errors - 'integration_tests' is already included in the default config
     extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs', 'e2e/playwright/**'],
   }),
   {
