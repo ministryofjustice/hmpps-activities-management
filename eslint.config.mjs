29f7ecd24f6beaadc9037743dea279db65d77b97
spennymoj <spencer.howe@justice.gov.uk>
2026-08-12 10:45:53 +0100

    feat: set up playwright and migrate deallocateFromActivity from cy to spec (#2310)
    * feat: set up playwright with representative smoke test
    
    * feat: resolve allowed scripts
    
    * feat: fix lint, add playwright to pipeline
    
    * feat: tighten cypress setup, improve linter, conditional install for missing cache
    
    * feat: amend how the pipeline accesses cache
    
    * feat: migrate deallocateFromActivity.cy.ts to deallocateFromActivity.spec.ts
    
    * feat: linting changes
    
    * feat: expand deallocateFromActivity to cover validations
    
    * feat: remove now replaced deallocateFromActivity.cy.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,8 @@ import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 export default [
   ...hmppsConfig({
     extraIgnorePaths: ['assets', 'frontend', 'cypress.json', 'reporter-config.json', 'server/@types'],
-    extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs'],
+    // HMPPS eslint config doesn't recognise e2e as a test folder, so we need to add it here to avoid linting errors - 'integration_tests' is already included in the default config
+    extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs', 'e2e/playwright/**'],
   }),
   {
     rules: {
