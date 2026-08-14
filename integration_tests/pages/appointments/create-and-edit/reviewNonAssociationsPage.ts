4b3aefd7c00bac81195b305c8f59eafb461a5996
spencer.howe <spencer.howe@justice.gov.uk>
2026-08-14 20:48:00 +0100

    feat: fix copy paste error from restore

--- integration_tests/pages/appointments/create-and-edit/reviewNonAssociationsPage.ts
+++ integration_tests/pages/appointments/create-and-edit/reviewNonAssociationsPage.ts
@@ -1,83 +1,19 @@
-import { format } from 'date-fns'
 import Page from '../../page'
 
-export default class CheckAnswersPage extends Page {
+export default class ReviewNonAssociationsPage extends Page {
   constructor() {
-    super('appointment-check-answers-page')
+    super('appointment-review-non-associations-page')
   }
 
-  assertAppointmentDetail = (header: string, value: string) =>
-    this.assertSummaryListValue('appointment-details', header, value)
+  header = () => cy.get('h1')
 
-  assertScheduleDetail = (header: string, value: string) =>
-    this.assertSummaryListValue('scheduling-information', header, value)
+  attendeeParagraph = () => cy.get('[data-qa="attendee-numbers"]')
 
-  assertPrisonerSummary = (name: string, number: string, cellLocation: string) => {
-    cy.get('[data-qa=prisoner-name]').contains(name)
-    cy.get('[data-qa=prisoner-number]').contains(number)
-    cy.get('[data-qa=prisoner-cell-location]').contains(cellLocation)
-  }
+  removeAttendeeLink = (prisonerNumber: string) => cy.get(`[data-qa=remove-attendee-link-${prisonerNumber}]`)
 
-  assertPrisonerInList = (name: string, number: string) => {
-    cy.get('[data-qa=prisoner-name]')
-      .contains(name)
-      .parent()
-      .parent()
-      .find('[data-qa=prisoner-number]')
-      .contains(number)
-  }
+  cards = (number: number) => cy.get('.govuk-summary-card').should('have.length', number)
 
-  assertCategory = (category: string) => this.assertAppointmentDetail('Appointment name', category)
+  getCard = (prisonerNumber: string) => cy.get(`[data-qa=card-${prisonerNumber}]`).find('td')
 
-  assertTier = (tier: string) => this.assertAppointmentDetail('Tier', tier)
-
-  assertHost = (host: string) => this.assertAppointmentDetail('Host', host)
-
-  assertLocation = (location: string) => this.assertScheduleDetail('Location', location)
-
-  assertStartDate = (startDate: Date) => this.assertScheduleDetail('Date', format(startDate, 'EEEE, d MMMM yyyy'))
-
-  assertStartTime = (hour: number, minute: number) =>
-    this.assertScheduleDetail('Start time', `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`)
-
-  assertEndTime = (hour: number, minute: number) =>
-    this.assertScheduleDetail('End time', `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`)
-
-  assertRepeat = (option: string) => this.assertScheduleDetail('Repeats', option)
-
-  assertFrequency = (option: string) => this.assertScheduleDetail('Frequency', option)
-
-  assertNumberOfAppointments = (option: string) => this.assertScheduleDetail('Number of appointments', option)
-
-  changePrisoners = () => cy.get('[data-qa=change-prisoners]').click()
-
-  changeName = () => cy.get('[data-qa=change-name]').click()
-
-  changeTier = () => cy.get('[data-qa=change-tier]').click()
-
-  changeHost = () => cy.get('[data-qa=change-host]').click()
-
-  changeLocation = () => cy.get('[data-qa=change-location]').click()
-
-  changeStartDate = () => cy.get('[data-qa=change-start-date]').click()
-
-  changeStartTime = () => cy.get('[data-qa=change-start-time]').click()
-
-  changeEndTime = () => cy.get('[data-qa=change-end-time]').click()
-
-  changeRepeat = () => cy.get('[data-qa=change-repeat]').click()
-
-  changeFrequency = () => cy.get('[data-qa=change-frequency]').click()
-
-  changeNumberOfAppointments = () => cy.get('[data-qa=change-number-of-appointments]').click()
-
-  changeExtraInformation = () => cy.get('[data-qa=change-extra-information]').click()
-
-  createAppointment = () => cy.get('button').contains('Confirm').click()
-
-  assertStaffNotes = (notes: string) =>
-    cy.get('span[data-qa=staff-notes]').should($el => expect($el.text().trim()).to.equal(notes))
-
-  assertPrisonerNotes = (notes: string) =>
-    cy.get('span[data-qa=prisoner-notes]').should($el => expect($el.text().trim()).to.equal(notes))
+  remainingAttendees = () => cy.get('[data-qa=remaining-attendees]')
 }
